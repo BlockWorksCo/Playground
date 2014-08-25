2cf9014a5ff857eecaad30cff083773a9e390ed3
Steve Tickle <Steve@ZooKoo.com>
2014-08-25 22:17:41 +0100

    - Working on the variadic container.

--- Dispatch.cpp
+++ Dispatch.cpp
@@ -119,6 +119,62 @@ template <typename First, typename EnumerationType, typename... Rest> void Enume
 
 
 
+template <typename... T>
+class Container
+{
+
+public:
+
+    Container(T... values)
+    {
+        Store(values...);
+    }
+
+    void ShowAll()
+    {
+        Show<T...>();
+    }
+
+private:
+
+    //
+    //
+    //
+    template <typename First> void Store(First& first) 
+    {
+        storage[0]   = &first;
+    }
+
+    template <typename First, typename... Rest> void Store(First& first, Rest&... rest) 
+    {
+        storage[sizeof...(Rest)]   = &first;
+        Store(rest...);
+    }
+
+    //
+    //
+    //
+    template <typename First> void Show() 
+    {
+        storage[0]   = (void*)1;
+    }
+
+    template <typename First, typename Second, typename... Rest> void Show() 
+    {
+        storage[sizeof...(Rest)]   = (void*)1;
+        Show<Second,Rest...>();
+    }
+
+
+    void*       storage[sizeof...(T)];
+
+};
+
+
+
+
+
+
 
 #define NUMBER_OF_ELEMENTS(a)   sizeof(a)/sizeof(a[0])
 
@@ -133,11 +189,17 @@ typedef enum
 
 int main()
 {
-    One     one;
-    Two     two;
-    Three   three;
+    One         one;
+    Two         two;
+    Three       three;
+    uint8_t     a0  = 1;
+    uint16_t    a1  = 2;
+    uint32_t    a2  = 3;
 
     //IndexedDispatch(0,  one,two,three);
+
+    Container<uint8_t, uint16_t, uint32_t>  container( a0,a1,a2 );
+    container.ShowAll();
 
 
     volatile ObjectType  order[]     = {eThree, eTwo, eOne};
