54c64d16564ffa9b5204591181c8af0bb3eeb025
Steve Tickle <Steve@ZooKoo.com>
2014-08-26 22:54:27 +0100

    - Refactored to use method pointers rather than function pointers.

--- DelayedDispatch.cpp
+++ DelayedDispatch.cpp
@@ -6,7 +6,9 @@
 
 
 
-
+//
+//
+//
 template<int ...> struct seq {};
 
 template<int N, int ...S> struct gens : gens<N-1, N-1, S...> {};
@@ -14,20 +16,15 @@ template<int N, int ...S> struct gens : gens<N-1, N-1, S...> {};
 template<int ...S> struct gens<0, S...>{ typedef seq<S...> type; };
 
 
-
-int foo(int x, float y, double z)
-{
-    printf("%d %f %f\n",x,y,z);
-    return x + y + z;
-}
-
-
-
-template < typename ReturnType, typename ...Args>
+//
+//
+//
+template < typename ReturnType, typename TargetType, typename ...Args>
 struct Delegate
 {
-    std::tuple<Args...>   params;
-    ReturnType            (*func)(Args...);
+    std::tuple<Args...>     params;
+    ReturnType              (TargetType::*func)(Args...);
+    TargetType&             targetInstance;
 
     ReturnType operator()()
     {
@@ -37,16 +34,115 @@ struct Delegate
     template<int ...S>
     ReturnType callFunc(seq<S...>)
     {
-        return func(std::get<S>(params) ...);
+        return (targetInstance.*func)(std::get<S>(params) ...);
     }
 };
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class One
+{
+public:
+    void operator()()
+    {
+        printf("One.\n");
+    }
+
+    void DoSomething()
+    {
+        printf("<One>.\n");
+    }
+
+    int DoAnotherThing(int p0)
+    {
+        return 11;
+    }
+
+    int foo(int x, float y, double z)
+    {
+        printf("%d %f %f\n",x,y,z);
+        return x + y + z;
+    }
+
+};
+
+
+
+class Two
+{
+public:
+    void operator()()
+    {
+        printf("Two.\n");
+    }
+
+    void DoSomething()
+    {
+        printf("<Two>.\n");
+    }
+
+    int DoAnotherThing(int p0)
+    {
+        return 22;
+    }
+};
+
+
+
+class Three
+{
+public:
+    void operator()()
+    {
+        printf("Three.\n");
+    }
+
+    void DoSomething()
+    {
+        printf("<Three>.\n");
+    }
+
+    int DoAnotherThing(int p0)
+    {
+        return 33;
+    }
+};
+
+
+
+
+
+
+//
+//
+//
 int main(void)
 {
-    std::tuple<int, float, double>  t         = std::make_tuple(1, 1.2, 5);
-    Delegate<int, int,float, double>     delegate  = {t, foo};
+    One                                     one;
+    Two                                     two;
+    Three                                   three;    
+    std::tuple<int, float, double>          params    = std::make_tuple(1, 1.2, 5);
+    Delegate<int, One, int,float, double>   delegate  = {params, &One::foo, one};
 
     //
     //
