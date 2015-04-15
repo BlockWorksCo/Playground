d6d397f59d2030fd20b0ac155e41410c6801dc7c
Steve Tickle <Steve@ZooKoo.com>
2015-04-15 23:36:19 +0100

    -

--- Symbols.py
+++ Symbols.py
@@ -5,18 +5,8 @@
 
 
 
-timestamp   = 0
-def CurrentTimestamp():
-    """
-    """
-    return timestamp + 1
 
 
-
-
-
-
-symbols             = set( [0] )
 links               = {}
 
 liveWorkingMemory   = []
@@ -28,9 +18,6 @@ modelWorkingMemory  = []
 def StrengthenLink( links, symbolA, symbolB ):
     """
     """
-    symbols.add( symbolA )
-    symbols.add( symbolB )
-
     if symbolA in links:
         if symbolB in links[symbolA]:
             links[symbolA][symbolB]     = links[symbolA][symbolB]+1
@@ -44,9 +31,6 @@ def StrengthenLink( links, symbolA, symbolB ):
 def DecayLink( links, symbolA, symbolB ):
     """
     """
-    symbols.add( symbolA )
-    symbols.add( symbolB )
-
     if symbolA in links:
         if symbolB in links[symbolA]:
             links[symbolA][symbolB]     = links[symbolA][symbolB]-1
@@ -92,13 +76,11 @@ StrengthenLink( links, 1, 3 )
 StrengthenLink( links, 2, 1 )
 StrengthenLink( links, 2, 0 )
 
-print(symbols)
 print(links)
 
 StrengthenLink( links, 0, 1 )
 StrengthenLink( links, 2, 0 )
 
-print(symbols)
 print(links)
 
 
