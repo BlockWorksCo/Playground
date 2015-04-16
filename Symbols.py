26a1150423132b8a0bd99852121bb9621f4fbd0d
Steve Tickle <Steve@ZooKoo.com>
2015-04-16 07:12:59 +0100

    -

--- Symbols.py
+++ Symbols.py
@@ -5,61 +5,66 @@
 
 
 
-
-
-links               = {}
-
-liveWorkingMemory   = []
-modelWorkingMemory  = []
-
-
-
-
-def StrengthenLink( links, symbolA, symbolB ):
+class Generation:
     """
     """
-    if symbolA in links:
-        if symbolB in links[symbolA]:
-            links[symbolA][symbolB]     = links[symbolA][symbolB]+1
+
+    links               = {}
+
+    liveWorkingMemory   = []
+    modelWorkingMemory  = []
+
+
+    def __init__(self):
+        """
+        """
+        pass
+
+
+    def StrengthenLink( self, links, symbolA, symbolB ):
+        """
+        """
+        if symbolA in links:
+            if symbolB in links[symbolA]:
+                links[symbolA][symbolB]     = links[symbolA][symbolB]+1
+            else:
+                links[symbolA][symbolB]     = 1
         else:
-            links[symbolA][symbolB]     = 1
-    else:
-        links[symbolA] = {symbolB:1}
+            links[symbolA] = {symbolB:1}
 
 
 
-def DecayLink( links, symbolA, symbolB ):
-    """
-    """
-    if symbolA in links:
-        if symbolB in links[symbolA]:
-            links[symbolA][symbolB]     = links[symbolA][symbolB]-1
+    def DecayLink( self, links, symbolA, symbolB ):
+        """
+        """
+        if symbolA in links:
+            if symbolB in links[symbolA]:
+                links[symbolA][symbolB]     = links[symbolA][symbolB]-1
+            else:
+                links[symbolA][symbolB]     = 1
         else:
-            links[symbolA][symbolB]     = 1
-    else:
-        links[symbolA] = {symbolB:1}
+            links[symbolA] = {symbolB:1}
 
 
 
 
-def Cycle( memory, model ):
-    """
-    """
+    def Cycle( self, memory, model ):
+        """
+        """
 
-    #
-    # Strengthen the links between symbols in working memory.
-    #
-    for symbolFrom in memory:
-        for symbolTo in memory:
-            StrengthenLink( symbolFrom, symbolTo )
+        #
+        # Strengthen the links between symbols in working memory.
+        #
+        for symbolFrom in memory:
+            for symbolTo in memory:
+                StrengthenLink( symbolFrom, symbolTo )
 
-    #
-    # Decay links between all symbols
-    #
-    for symbolFrom in symbols:
-        for symbolTo in symbols:
-            DecayLinks( symbolFrom, symbolTo )
-
+        #
+        # Decay links between all symbols
+        #
+        for symbolFrom in symbols:
+            for symbolTo in symbols:
+                DecayLinks( symbolFrom, symbolTo )
 
 
 
@@ -68,20 +73,23 @@ def Cycle( memory, model ):
 
 
 
-StrengthenLink( links, 0, 1 )
-StrengthenLink( links, 0, 2 )
-StrengthenLink( links, 0, 3 )
-StrengthenLink( links, 1, 2 )
-StrengthenLink( links, 1, 3 )
-StrengthenLink( links, 2, 1 )
-StrengthenLink( links, 2, 0 )
+generation0     = Generation()
 
-print(links)
 
-StrengthenLink( links, 0, 1 )
-StrengthenLink( links, 2, 0 )
+generation0.StrengthenLink( generation0.links, 0, 1 )
+generation0.StrengthenLink( generation0.links, 0, 2 )
+generation0.StrengthenLink( generation0.links, 0, 3 )
+generation0.StrengthenLink( generation0.links, 1, 2 )
+generation0.StrengthenLink( generation0.links, 1, 3 )
+generation0.StrengthenLink( generation0.links, 2, 1 )
+generation0.StrengthenLink( generation0.links, 2, 0 )
 
-print(links)
+print(generation0.links)
+
+generation0.StrengthenLink( generation0.links, 0, 1 )
+generation0.StrengthenLink( generation0.links, 2, 0 )
+
+print(generation0.links)
 
 
 
