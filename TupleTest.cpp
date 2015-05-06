2c6ff3351d15a0233df8f4fbdcbbf86eed939e42
Steve Tickle <Steve@ZooKoo.com>
2015-05-06 22:50:28 +0100

    -

--- TupleTest.cpp
+++ TupleTest.cpp
@@ -7,97 +7,20 @@
 #include <utility>
 
 
+//
+//
+//
+template<std::size_t I = 0, typename... Tp>
+typename std::enable_if<I == sizeof...(Tp), void>::type CallEach(std::tuple<Tp...>& t)
+{ 
+}
 
-
-
-#if 0
-
-template <typename ReturnType, typename... T>
-class Container
+template<std::size_t I = 0, typename... Tp>
+typename std::enable_if<I < sizeof...(Tp), void>::type CallEach(std::tuple<Tp...>& t)
 {
-
-public:
-
-    Container(T... values)
-    {
-        Store(values...);
-    }
-
-    template <typename... ParamTypes> ReturnType Call(uint8_t index, ParamTypes... params)
-    {
-        return Call<T...>(index);
-    }
-
-private:
-
-    //
-    //
-    //
-    template <typename First> void Store(First& first) 
-    {
-        storage[0]   = &first;
-    }
-
-    template <typename First, typename... Rest> void Store(First& first, Rest&... rest) 
-    {
-        storage[sizeof...(Rest)]   = &first;
-        Store(rest...);
-    }
-
-    //
-    //
-    //
-    template <typename First> ReturnType Call(uint8_t index) 
-    {
-        if( index == (sizeof...(T)-1) ) 
-        {
-            First*  pFirst  = (First*)storage[0];
-            First   value   = *pFirst;
-            return value(0);
-        }
-    }
-
-    template <typename First, typename Second, typename... Rest> ReturnType Call(uint8_t index) 
-    {
-        if( index == (sizeof...(T) - (sizeof...(Rest)+1)) - 1)
-        {
-            First*  pFirst  = (First*)storage[sizeof...(Rest)+1];
-            First   value   = *pFirst;
-            return value(0);
-        }
-        else
-        {            
-            return Call<Second,Rest...>(index);
-        }
-    }
-
-private:
-
-    const void*       storage[sizeof...(T)];
-
-};
-
-
-
-Container<int,  [](int p0)  {printf("[One:%d]\n",p0);},
-                [](int p0)  {printf("[Two:%d]\n",p0);},
-                [](int p0)  {printf("[Three:%d]\n",p0);} >  containerA();
-
-
-#endif
-
-template<std::size_t I = 0, typename... Tp>
-inline typename std::enable_if<I == sizeof...(Tp), void>::type
-  print(std::tuple<Tp...>& t)
-  { }
-
-template<std::size_t I = 0, typename... Tp>
-inline typename std::enable_if<I < sizeof...(Tp), void>::type
-  print(std::tuple<Tp...>& t)
-  {
     std::get<I>(t)();
-    print<I + 1, Tp...>(t);
-  }
+    CallEach<I + 1, Tp...>(t);
+}
 
 
 
@@ -107,14 +30,14 @@ inline typename std::enable_if<I < sizeof...(Tp), void>::type
 int main()
 {
 
-    auto protocol = std::make_tuple(
+    auto container = std::make_tuple(
             []()  {printf("[One:]\n");},
             []()  {printf("[Two:]\n");},
             []()  {printf("[Three:]\n");}
         );
  
 
-    print( protocol );
+    CallEach( container );
 }
 
 
