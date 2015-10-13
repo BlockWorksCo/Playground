688cc44398aeb7fd14af37a489e9aae906de80c7
Steve Tickle <Steve@ZooKoo.com>
2015-10-13 21:28:32 +0100

    -

---
+++ ReactorCore/UARTTransmit.h
@@ -0,0 +1,52 @@
+
+
+
+#ifndef __UARTTRANSMIT_H__
+#define __UARTTRANSMIT_H__
+
+template <  typename pinType, 
+            typename timingType, 
+            uint32_t ticksPerSecond>
+class UARTTransmit
+{
+
+public:
+
+
+    UARTTransmit( pinType& _pin, timingType& _timing ) :
+            pin(_pin),
+            timing(_timing),
+            state(false)
+    {
+        pin.Set();
+    }
+
+
+    void Process()
+    {
+        if( (timing.GetTick()%ticksPerSecond) <= 100)
+        {
+            if(state == true)
+            {
+                pin.Set();            
+            }
+            else
+            {
+                pin.Clear();            
+            }            
+        }
+    }
+
+private:
+
+    pinType&    pin;
+    timingType& timing;
+    bool        state;
+
+};
+
+
+
+
+#endif
+
