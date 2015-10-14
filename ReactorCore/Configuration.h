f0ef2a1e00d8378c00dde80b604526a1ce2d9e1b
Steve Tickle <Steve@ZooKoo.com>
2015-10-14 06:54:17 +0100

    - Added Clock.h and the missing Configuration.h

---
+++ ReactorCore/Configuration.h
@@ -0,0 +1,62 @@
+
+
+
+#ifndef __CONFIGURATION_H__
+#define __CONFIGURATION_H__
+
+
+#include "GPIO.h"
+#include "CycleCounterTiming.h"
+
+#include "Toggle.h"
+#include "UARTTransmit.h"
+#include "UARTReceive.h"
+#include "Clock.h"
+
+
+//
+// Timing related definitions.
+//
+const uint32_t  CLOCK_RATE              = 200000000;
+typedef Timing<uint32_t, CLOCK_RATE>        TimingType;
+
+
+typedef STM32Output<GPIOD_BASE, 0>            Pin0Type;
+typedef STM32Output<GPIOD_BASE, 1>            Pin1Type;
+typedef STM32Input<GPIOD_BASE, 2>             Pin2Type;
+typedef STM32Output<GPIOD_BASE, 3>            Pin3Type;
+typedef STM32Output<GPIOD_BASE, 4>            Pin4Type;
+typedef STM32Output<GPIOD_BASE, 5>            Pin5Type;
+typedef STM32Output<GPIOD_BASE, 6>            Pin6Type;
+typedef STM32Output<GPIOD_BASE, 7>            Pin7Type;
+
+
+
+extern TimingType                                  timing;
+extern Pin0Type                    pin0;
+extern Pin1Type                    pin1;
+extern Pin2Type                    pin2;
+extern Pin3Type                    pin3;
+extern Pin4Type                    pin4;
+extern Pin5Type                    pin5;
+extern Pin6Type                    pin6;
+extern Pin7Type                    pin7;
+
+
+
+ToggleController<Pin0Type, TimingType, CLOCK_RATE>              pin0Controller(pin0, timing);
+ToggleController<Pin1Type, TimingType, CLOCK_RATE>              pin1Controller(pin1, timing);
+UARTReceive<Pin2Type, TimingType, CLOCK_RATE, 9600,8,10,8>      pin2Controller(pin2, timing);
+UARTTransmit<Pin3Type, TimingType, CLOCK_RATE, 9600,8,100>      pin3Controller(pin3, timing);
+ToggleController<Pin4Type, TimingType, CLOCK_RATE>              pin4Controller(pin4, timing);
+ToggleController<Pin5Type, TimingType, CLOCK_RATE>              pin5Controller(pin5, timing);
+ToggleController<Pin6Type, TimingType, CLOCK_RATE>              pin6Controller(pin6, timing);
+Clock<Pin7Type, TimingType, CLOCK_RATE, 1000>                   pin7Controller(pin7, timing);
+
+
+
+#endif
+
+
+
+
