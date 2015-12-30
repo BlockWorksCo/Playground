e73f3b0bb4b5ad37716058f1df396a34fd159cd4
Steve Tickle <Steve@ZooKoo.com>
2015-12-30 23:28:35 +0000

    - Code visibile ono other cores but relocated to virtual instead of physical adddress.

--- ReactorCore/CoreLoader/main.c
+++ ReactorCore/CoreLoader/main.c
@@ -1,3 +1,6 @@
+
+
+
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -16,57 +19,60 @@
 ELFSymbol_t     exports[]   = {0};
 ELFEnv_t        env         = { exports, 0 };
 
-int main(int argc, char* argv[])
+
+//
+//
+//
+uint32_t get_page_frame_number_of_address(void* addr)
 {
-    exec_elf(argv[1], &env);
-    puts("Done");
-    return 0;
+    uint32_t    returnValue     = (uint32_t)-1;
+
+    // Open the pagemap file for the current process
+    FILE* pagemap = fopen("/proc/self/pagemap", "rb");
+    if(pagemap != NULL)
+    {
+        // Seek to the page that the buffer is on
+        unsigned long offset = (unsigned long)addr / getpagesize() * PAGEMAP_LENGTH;
+
+        if(fseek(pagemap, (unsigned long)offset, SEEK_SET) != 0)
+        {
+            fprintf(stderr, "Failed to seek pagemap to proper location\n");
+            exit(1);
+        }
+
+        // The page frame number is in bits 0-54 so read the first 7 bytes and clear the 55th bit
+        uint32_t    page_frame_number = 0;
+        fread(&page_frame_number, 1, PAGEMAP_LENGTH - 1, pagemap);
+        page_frame_number &= 0x7FFFFFFFFFFFFF;
+        fclose(pagemap);
+
+        returnValue     = page_frame_number;
+    }
+    else
+    {
+        perror("Couldn't open pagemap.\n");
+        exit(-1);
+    }
+
+    return returnValue;
 }
 
-//
-//
-//
-unsigned long get_page_frame_number_of_address(void *addr) 
-{
-   // Open the pagemap file for the current process
-   FILE *pagemap = fopen("/proc/self/pagemap", "rb");
-
-   // Seek to the page that the buffer is on
-   unsigned long offset = (unsigned long)addr / getpagesize() * PAGEMAP_LENGTH;
-   if(fseek(pagemap, (unsigned long)offset, SEEK_SET) != 0) 
-   {
-      fprintf(stderr, "Failed to seek pagemap to proper location\n");
-      exit(1);
-   }
-
-   // The page frame number is in bits 0-54 so read the first 7 bytes and clear the 55th bit
-   unsigned long page_frame_number = 0;
-   fread(&page_frame_number, 1, PAGEMAP_LENGTH-1, pagemap);
-
-   page_frame_number &= 0x7FFFFFFFFFFFFF;
-
-   fclose(pagemap);
-
-   return page_frame_number;
-}
-
 
 //
 //
 //
-void *do_alloc(size_t size, size_t align, ELFSecPerm_t perm)
+void* do_alloc(size_t size, size_t align, ELFSecPerm_t perm)
 {
     (void) perm;
-    
     //
     // Allocate the block, aligned on page size..
     //
     void*   block   = memalign(32768, size);
-
     //
     // Set the attributes to allow execution.
     //
-    int     r       = mprotect(block, size, PROT_EXEC|PROT_READ|PROT_WRITE);
+    int     r       = mprotect(block, size, PROT_EXEC | PROT_READ | PROT_WRITE);
+
     if(r != 0)
     {
         perror("mprotect failed.");
@@ -77,6 +83,7 @@ void *do_alloc(size_t size, size_t align, ELFSecPerm_t perm)
     // Lock it in physical RAM.
     //
     r   = mlock(block, size);
+
     if(r == -1)
     {
         perror("mlock failed.");
@@ -89,37 +96,33 @@ void *do_alloc(size_t size, size_t align, ELFSecPerm_t perm)
     uint32_t    pageFrameNumber     = get_page_frame_number_of_address( block );
     uint32_t    physicalAddress     = (pageFrameNumber << PAGE_SHIFT) + 0;
 
+    printf("physical address = %08x virtual address = %08x\n", physicalAddress, (uint32_t)block );
+
     return block;
 }
 
+
+//
+//
+//
 void arch_jumpTo(entry_t entry)
 {
-    static uint8_t  temp[100]   = {0};
-    register uint32_t        r11         = (uint32_t)&temp[50];
-    __asm__ volatile("MOV r11, %0\n\t" : : "r"(r11));
+    printf("EntryPoint @ %08x \n", (uint32_t)entry );
     entry();
-#if 0
-    void *stack = do_alloc(APP_STACK_SIZE, 8, ELF_SEC_READ | ELF_SEC_WRITE);
-    if (stack) {
-        register uint32_t saved = 0;
-        void *tos = stack + APP_STACK_SIZE;
-
-        /* s->saved */
-        __asm__ volatile("MOV %0, sp\n\t" : : "r"(saved));
-        /* tos->MSP */
-        __asm__ volatile("MOV sp, %0\n\t" : : "r"(tos));
-        /* push saved */
-        __asm__ volatile("PUSH {%0}\n\t" : : "r"(saved));
-
-        entry();
-
-        /* pop saved */
-        __asm__ volatile("POP {%0}\n\t" : : "r"(saved));
-        /* saved->sp */
-        __asm__ volatile("MOV sp, %0\n\t" : : "r"(saved));
-
-            free(stack);
-        } else
-            perror("Stack alloc");
-#endif      
 }
+
+
+
+
+
+//
+//
+//
+int main(int argc, char* argv[])
+{
+    exec_elf(argv[1], &env);
+    puts("Done");
+    return 0;
+}
+
+
--- ReactorCore/EdgeDetector/Main.c
+++ ReactorCore/EdgeDetector/Main.c
@@ -4,10 +4,12 @@
 #include <stdbool.h>
 
 
+//
+//
+//
 
 
-
-void DoSomething()
+void CoreMain()
 {
     static volatile uint32_t     counter     = 0;
     while(true)    
@@ -16,10 +18,20 @@ void DoSomething()
     }    
 }
 
+uint8_t      stack[1024*1];
 
 void EntryPoint()
 {
-    DoSomething();
+    //
+    // Setup the stack.
+    //
+    //uint32_t            stackPointer    = ((uint32_t)&stack[0]) + sizeof(stack);
+    //__asm__ volatile("MOV sp, %0\n\t" : : "r"(stackPointer));
+
+    //
+    // Call the CoreMain.
+    //
+    CoreMain();
 }
 
 
