d74037fe665777a1060a0dfcd7695919ca837fbc
Steve Tickle <Steve@ZooKoo.com>
2016-01-02 23:47:59 +0000

    - First trigger of another core....

--- ReactorCore/CoreServices/Test.c
+++ ReactorCore/CoreServices/Test.c
@@ -16,36 +16,19 @@
 #include <fcntl.h>      /* open */
 #include <unistd.h>     /* exit */
 #include <sys/ioctl.h>      /* ioctl */
+#include <ctype.h>
 
-
-
-
-
-/*
- * Functions for the ioctl calls
- */
-
-void ioctl_set_msg(int file_desc, char* message)
-{
-    int ret_val;
-    ret_val = ioctl(file_desc, IOCTL_SET_MSG, message);
-
-    if (ret_val < 0)
-    {
-        printf("ioctl_set_msg failed:%d\n", ret_val);
-        exit(-1);
-    }
-}
 
 
 
 /*
  * Main - Call the ioctl functions
  */
-int main()
+int main(int argc, char* argv[])
 {
     int file_desc, ret_val;
     char* msg = "Message passed by ioctl\n";
+
     file_desc = open(DEVICE_FILE_NAME, 0);
 
     if (file_desc < 0)
@@ -54,6 +37,20 @@ int main()
         exit(-1);
     }
 
-    ioctl_set_msg(file_desc, msg);
+    if(argc == 2)
+    {
+        int ret_val;
+        ret_val = ioctl(file_desc, IOCTL_SET_MSG, atoi(argv[1]) );
+
+        if (ret_val < 0)
+        {
+            printf("ioctl_set_msg failed:%d\n", ret_val);
+            exit(-1);
+        }
+    }
+    else
+    {
+        printf("no args.\n");
+    }
     close(file_desc);
 }
