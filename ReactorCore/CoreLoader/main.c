59dd5308193d938838e02f7db409c02879645513
Steve Tickle <Steve@ZooKoo.com>
2016-02-10 19:46:02 +0000

    -

--- ReactorCore/CoreLoader/main.c
+++ ReactorCore/CoreLoader/main.c
@@ -31,6 +31,7 @@
 #define PAGEMAP_LENGTH  8
 #define PAGE_SHIFT      12
 
+#define PERIPHERALS_RAM_BASE    (0x4000008c)
 
 #define dsb(option) asm volatile ("dsb " #option : : : "memory")
 
@@ -47,105 +48,15 @@ int             memFD       = -1;
 uint8_t*        alloyRAM    = 0;
 uint8_t*        topOfHeap   = 0;
 
+int             peripheralsFD   = -1;
+uint8_t*        peripherals     = 0;
 
-#if 0
-
-//
-//
-//
-uint32_t get_page_frame_number_of_address(void* addr)
-{
-    uint32_t    returnValue     = (uint32_t)-1;
-
-    // Open the pagemap file for the current process
-    FILE* pagemap = fopen("/proc/self/pagemap", "rb");
-    if(pagemap != NULL)
-    {
-        // Seek to the page that the buffer is on
-        unsigned long offset = (unsigned long)addr / getpagesize() * PAGEMAP_LENGTH;
-
-        if(fseek(pagemap, (unsigned long)offset, SEEK_SET) != 0)
-        {
-            fprintf(stderr, "Failed to seek pagemap to proper location\n");
-            exit(1);
-        }
-
-        // The page frame number is in bits 0-54 so read the first 7 bytes and clear the 55th bit
-        uint32_t    page_frame_number = 0;
-        fread(&page_frame_number, 1, PAGEMAP_LENGTH - 1, pagemap);
-        page_frame_number &= 0x7FFFFFFFFFFFFF;
-        fclose(pagemap);
-
-        returnValue     = page_frame_number;
-    }
-    else
-    {
-        perror("Couldn't open pagemap.\n");
-        exit(-1);
-    }
-
-    return returnValue;
-}
-
-//
-//
-//
-uint32_t PhysicalAddressOf(uint32_t virtualAddress)
-{
-    uint32_t pageFrameNumber        = get_page_frame_number_of_address( (void*)virtualAddress );
-    const uint32_t  pageSize        = 1 << PAGE_SHIFT;
-    uint32_t offsetIntoPage         = virtualAddress % pageSize;
-    uint32_t physicalAddressOfPage  = pageFrameNumber << PAGE_SHIFT;
-    uint32_t physicalAddress        = physicalAddressOfPage + offsetIntoPage;
-
-    return physicalAddressOfPage;
-}
-#endif
 
 //
 //
 //
 void* do_alloc(size_t size, size_t align, ELFSecPerm_t perm, uint32_t* physicalAddress)
 {
-#if 0    
-    (void) perm;
-    //
-    // Allocate the block, aligned on page size..
-    //
-    void*   block   = memalign(32768, size);
-    //
-    // Set the attributes to allow execution.
-    //
-    int     r       = mprotect(block, size, PROT_EXEC | PROT_READ | PROT_WRITE);
-
-    if(r != 0)
-    {
-        perror("mprotect failed.");
-        exit(-1);
-    }
-
-    //
-    // Lock it in physical RAM.
-    //
-    r   = mlock(block, size);
-
-    if(r == -1)
-    {
-        perror("mlock failed.");
-        exit(-1);
-    }
-
-    //
-    // Get the physical address of this virtual block address.
-    //
-    uint32_t    pageFrameNumber     = get_page_frame_number_of_address( block );
-    *physicalAddress     = (pageFrameNumber << PAGE_SHIFT) + 0;
-
-    printf("physical address = %08x virtual address = %08x size=%d\n", *physicalAddress, (uint32_t)block, size );
-
-    return block;
-#else
-
     //
     //
     //
@@ -159,98 +70,9 @@ void* do_alloc(size_t size, size_t align, ELFSecPerm_t perm, uint32_t* physicalA
     memset(block, 0, size);
 
     return block;
-
-#endif    
-}
-#if 0
-
-//
-//
-//
-static inline void dcache_clean(void)
- {
-     const int zero = 0;
-     /* clean entire D cache -> push to external memory. */
-     __asm volatile ("1: mrc p15, 0, r15, c7, c10, 3\n"
-                     " bne 1b\n" ::: "cc");
-     /* drain the write buffer */
-    __asm volatile ("mcr 15, 0, %0, c7, c10, 4"::"r" (zero));
- }
-
-#endif
-
-
-#if 0
-//
-// Cause the specified core to start execution at the specified point in physical memory.
-//
-void GetBridgeData( CoreServicesBridge* bridge )
-{
-#if 0
-    int     file_desc;
-
-    file_desc = open("/dev/ReactorCoreServices", 0);
-
-    if (file_desc < 0)
-    {
-        ERR("Can't open /dev/ReactorCoreServices");
-    }
-
-    int ret_val = ioctl(file_desc, IOCTL_GET_MSG, bridge );
-
-    if (ret_val < 0)
-    {
-        printf("ioctl failed:%d\n", ret_val);
-        exit(-1);
-    }
-    
-    close(file_desc);
-#else
-    
-    //
-    //
-    //
-    memcpy(bridge, (uint8_t*)ALLOY_RAM_BASE, sizeof(*bridge) );
-
-#endif    
-}
-#endif
-
-#if 0
-
-//
-// Cause the specified core to start execution at the specified point in physical memory.
-//
-void Allocate( uint32_t numberOfBytes )
-{
-    int     file_desc;
-
-    file_desc = open("/dev/ReactorCoreServices", 0);
-
-    if (file_desc < 0)
-    {
-        ERR("Can't open /dev/ReactorCoreServices");
-    }
-
-    AllocateRequest     request;
-    request.size            = numberOfBytes;
-    request.virtualAddress  = 0;
-    request.physicalAddress = 0;
-    int ret_val = ioctl(file_desc, IOCTL_ALLOCATE_BLOCK, &request );
-
-    if (ret_val < 0)
-    {
-        printf("ioctl failed:%d\n", ret_val);
-        exit(-1);
-    }
-    
-    close(file_desc);
-
-    printf("virtualAddress = %08x\n", request.virtualAddress);
-    printf("physicalAddress = %08x\n", request.physicalAddress);
 }
 
-#endif
+
 
 
 //
@@ -258,38 +80,20 @@ void Allocate( uint32_t numberOfBytes )
 //
 int GetBridgePhysicalAddress()
 {
-#if 0
-    int     file_desc;
-
-    file_desc = open("/dev/ReactorCoreServices", 0);
-
-    if (file_desc < 0)
-    {
-        ERR("Can't open /dev/ReactorCoreServices");
-    }
-
-    uint32_t physicalAddress    = 0;
-    int ret_val = ioctl(file_desc, IOCTL_GET_BRIDGE_ADDRESS, &physicalAddress );
-
-    if (ret_val < 0)
-    {
-        printf("ioctl failed:%d\n", ret_val);
-        exit(-1);
-    }
-    
-    close(file_desc);
-
-    return physicalAddress;
-#else
     return ALLOY_RAM_BASE;    
-#endif    
 }
 
 //
 // Cause the specified core to start execution at the specified point in physical memory.
 //
-int TriggerCoreExecution(uint32_t coreNumber, uint32_t physicalAddress)
+int TriggerCoreExecution(uint32_t coreID, uint32_t physicalAddress)
 {
+#if 0
+    uint32_t*   startTrigger    = (uint32_t*)(peripherals + 0x8c + (coreID<<4));
+
+    *startTrigger    = physicalAddress;
+    dsb();
+#else
     int     file_desc;
 
     file_desc = open("/dev/ReactorCoreServices", 0);
@@ -308,6 +112,7 @@ int TriggerCoreExecution(uint32_t coreNumber, uint32_t physicalAddress)
     }
     
     close(file_desc);
+#endif    
 }
 
 
@@ -368,128 +173,14 @@ void arch_jumpTo(entry_t entry)
     }
 }
 
-#if 0
-
-
-#define PAGE_SIZE 4096
-
-void Test()
-{
-    int configfd;
-    configfd = open("/dev/ReactorCoreServices", O_RDWR);
-    if(configfd < 0) 
-    {
-        perror("open");
-        return;
-    }
-
-    char* address = NULL;
-    address = mmap(NULL, PAGE_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, configfd, 0);
-    if (address == MAP_FAILED) 
-    {
-        perror("mmap");
-        return;
-    }
-
-    printf("initial message: %s\n", address);
-    memcpy(address + 11 , "*user*", 6);
-    printf("changed message: %s\n", address);
-    close(configfd);    
-}
-
-
-
-
-//
-// examine /proc/maps
-//
-void Test2()
-{
-    int fd;
-    fd = open("/dev/mem", O_RDWR|O_SYNC);
-    if(fd < 0) 
-    {
-        perror("open");
-        return;
-    }
-
-    //uint8_t*    ptr = mmap(ALLOY_RAM_BASE, getpagesize(), PROT_READ|PROT_WRITE, MAP_PRIVATE|MAP_FIXED , fd, ALLOY_RAM_BASE );
-    uint8_t*    ptr = mmap( (void*)ALLOY_RAM_BASE, getpagesize(), PROT_READ|PROT_WRITE, MAP_SHARED , fd, ALLOY_RAM_BASE );
-    printf("base = %08x\n", (uint32_t)ptr);
-    ptr[0]++;
-#if 0
-    for(uint32_t i=0; i<128; i++)
-    {
-        ptr[i]++;
-        printf("%02x ", ptr[i]);
-    }
-#endif
-    for(uint32_t i=0; i<128; i++)
-    {
-        printf("%02x ", ptr[i]);
-    }
-
-    printf("\n");
-    msync(ptr, 4096, MS_SYNC);
-    close(fd);
-
-    dsb(sy);
-}
-
-
-
-char temp[]     = "Hello World.";
-
-//
-// examine /proc/maps
-//
-void Test1()
-{
-    int fd;
-    fd = open("/dev/ReactorCoreServices", O_RDWR|O_SYNC);
-    if(fd < 0) 
-    {
-        perror("open");
-        return -1;
-    }
-
-    //uint8_t*    ptr = mmap(NULL, getpagesize(), PROT_READ|PROT_WRITE, MAP_PRIVATE , fd, ALLOY_RAM_BASE );
-    uint8_t*    ptr = mmap(NULL, getpagesize(), PROT_READ|PROT_WRITE, MAP_NORESERVE|MAP_PRIVATE , fd, 0 );
-    printf("base = %08x physical = %08x\n", ptr, PhysicalAddressOf((uint32_t)&temp[0]));
-    ptr[0]++;
-#if 0
-    for(uint32_t i=0; i<16; i++)
-    {
-        ptr[i] = i;
-        printf("%02x ", ptr[i]);
-    }
-#endif
-    for(uint32_t i=0; i<256; i++)
-    {
-        printf("%02x ", ptr[i]);
-    }
-
-    printf("\n");
-
-    dsb(sy);
-}
-
-#endif
-
 //
 //
 //
 int main(int argc, char* argv[])
 {
-#if 0    
-    Test2();
-    exit(0);
-
-    //Test2();
-    //exit(0);
-    Allocate(1024*1024);
-    exit(-1);
-#else
+    //
+    //
+    //
     memFD = open("/dev/mem", O_RDWR|O_SYNC);
     if(memFD < 0) 
     {
@@ -502,8 +193,22 @@ int main(int argc, char* argv[])
     memset(alloyRAM, 0, 4096);
     topOfHeap   = alloyRAM + 4096;
 
-#endif
+    //
+    //
+    //
+    peripheralsFD = open("/dev/mem", O_RDWR|O_SYNC);
+    if(peripheralsFD < 0) 
+    {
+        perror("open");
+        return -1;
+    }
 
+    peripherals = mmap( (void*)PERIPHERALS_RAM_BASE, 4096, PROT_READ|PROT_WRITE, MAP_SHARED , peripheralsFD, 0 );
+    printf("peripherals RAM base = %08x\n", (uint32_t)peripherals);
+
+    //
+    //
+    //
     exec_elf(argv[1], &env);
     puts("Done");
 
