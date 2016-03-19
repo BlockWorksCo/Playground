8f8cb97ea1b98f8ad0eef928fbb8b65b31935b6c
Steve Tickle <Steve@ZooKoo.com>
2016-03-19 21:58:26 +0000

    -

--- Update/Update.py
+++ Update/Update.py
@@ -11,6 +11,8 @@ ts 	= '%s'%datetime.datetime.now()
 t 	= open('find-ip-address').read()
 ip 	= re.compile('Your.*\>([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)').findall(t)[0]
 
+log 	= open('RecentSpeedTestHistory').read()
+log 	= log.replace('\n','<br>')
 
 template = """
 <html>
@@ -19,11 +21,13 @@ template = """
 <body>
 <p>%s</p>
 <p>%s</p>
+<br>
+<p><code>%s</code></p>
 </body>
 </html>
 """
 
-open('Home.html','w').write(template%(ip,ts))
+open('Home.html','w').write(template%(ip,ts,log))
 
 
 
--- Update/Update.sh
+++ Update/Update.sh
@@ -1,10 +1,21 @@
+#!/bin/bash
 
 
+timestamp=`date`
 
 cd /home/steve/Projects/Update
 rm -f find-ip-address
+rm SpeedTest
+
+speedtest-cli > SpeedTest
+up=`grep Upload SpeedTest `
+down=`grep Download SpeedTest `
+echo $timestamp $up $down >> SpeedTestHistory
+tail -20 SpeedTestHistory > RecentSpeedTestHistory
+
 wget https://www.iplocation.net/find-ip-address
 python Update.py 
+
 scp Home.html zookooco@zookoo.com:~/www
 
 
