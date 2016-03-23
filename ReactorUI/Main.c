3d6f443ffa492199ce6395e92ed8e353f614febd
Steve Tickle <Steve@ZooKoo.com>
2016-03-23 23:27:21 +0000

    -

--- ReactorUI/Main.c
+++ ReactorUI/Main.c
@@ -221,6 +221,27 @@ enum demo_protocols {
 
 
 
+const char * get_mimetype(const char *file)
+{
+    int n = strlen(file);
+
+    if (n < 5)
+        return NULL;
+
+    if (!strcmp(&file[n - 4], ".ico"))
+        return "image/x-icon";
+
+    if (!strcmp(&file[n - 4], ".png"))
+        return "image/png";
+
+    if (!strcmp(&file[n - 5], ".html"))
+        return "text/html";
+
+    if (!strcmp(&file[n - 4], ".css"))
+        return "text/css";
+
+    return NULL;
+}
 
 /* this protocol server (always the first one) handles HTTP,
  *
@@ -231,9 +252,7 @@ enum demo_protocols {
 int callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user,
           void *in, size_t len)
 {
-#if 0    
-    struct per_session_data__http *pss =
-            (struct per_session_data__http *)user;
+    struct per_session_data__http *pss = (struct per_session_data__http *)user;
     unsigned char buffer[4096 + LWS_PRE];
     unsigned long amount, file_len, sent;
     char leaf_path[1024];
@@ -242,22 +261,17 @@ int callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user,
     unsigned char *end;
     struct timeval tv;
     unsigned char *p;
-#ifndef LWS_NO_CLIENT
-    struct per_session_data__http *pss1;
-    struct lws *wsi1;
-#endif
     char buf[256];
     char b64[64];
     int n, m;
-#ifdef EXTERNAL_POLL
-    struct lws_pollargs *pa = (struct lws_pollargs *)in;
-#endif
+
 
     switch (reason) {
     case LWS_CALLBACK_HTTP:
+        printf("LWS_CALLBACK_HTTP\n");
 
         if (debug_level & LLL_INFO) {
-            dump_handshake_info(wsi);
+            //dump_handshake_info(wsi);
 
             /* dump the individual URI Arg parameters */
             n = 0;
@@ -281,85 +295,11 @@ int callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user,
             goto try_to_reuse;
         }
 
-#ifndef LWS_NO_CLIENT
-        if (!strncmp(in, "/proxytest", 10)) {
-            struct lws_client_connect_info i;
-            char *rootpath = "/";
-            const char *p = (const char *)in;
-
-            if (lws_get_child(wsi))
-                break;
-
-            pss->client_finished = 0;
-            memset(&i,0, sizeof(i));
-            i.context = lws_get_context(wsi);
-            i.address = "git.libwebsockets.org";
-            i.port = 80;
-            i.ssl_connection = 0;
-            if (p[10])
-                i.path = (char *)in + 10;
-            else
-                i.path = rootpath;
-            i.host = "git.libwebsockets.org";
-            i.origin = NULL;
-            i.method = "GET";
-            i.parent_wsi = wsi;
-            i.uri_replace_from = "git.libwebsockets.org/";
-            i.uri_replace_to = "/proxytest/";
-            if (!lws_client_connect_via_info(&i)) {
-                lwsl_err("proxy connect fail\n");
-                break;
-            }
-
-
-
-            break;
-        }
-#endif
-
-#if 1
         /* this example server has no concept of directories */
         if (strchr((const char *)in + 1, '/')) {
             lws_return_http_status(wsi, HTTP_STATUS_FORBIDDEN, NULL);
             goto try_to_reuse;
         }
-#endif
-
-#ifdef LWS_WITH_CGI
-        if (!strncmp(in, "/cgitest", 8)) {
-            static char *cmd[] = {
-                "/bin/sh",
-                "-c",
-                INSTALL_DATADIR"/libwebsockets-test-server/lws-cgi-test.sh",
-//              "/var/www/cgi-bin/cgit",
-                NULL
-            };
-
-            lwsl_notice("%s: cgitest\n", __func__);
-            n = lws_cgi(wsi, cmd, 8, 5);
-            if (n) {
-                lwsl_err("%s: cgi failed\n");
-                return -1;
-            }
-            p = buffer + LWS_PRE;
-            end = p + sizeof(buffer) - LWS_PRE;
-
-            if (lws_add_http_header_status(wsi, 200, &p, end))
-                return 1;
-            if (lws_add_http_header_by_token(wsi, WSI_TOKEN_CONNECTION,
-                    (unsigned char *)"close", 5, &p, end))
-                return 1;
-            n = lws_write(wsi, buffer + LWS_PRE,
-                      p - (buffer + LWS_PRE),
-                      LWS_WRITE_HTTP_HEADERS);
-
-            /* the cgi starts by outputting headers, we can't
-             *  finalize the headers until we see the end of that
-             */
-
-            break;
-        }
-#endif
 
         /* if a legal POST URL, let it continue and accept data */
         if (lws_hdr_total_length(wsi, WSI_TOKEN_POST_URI))
@@ -368,9 +308,7 @@ int callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user,
         /* check for the "send a big file by hand" example case */
 
         if (!strcmp((const char *)in, "/leaf.jpg")) {
-            if (strlen(resource_path) > sizeof(leaf_path) - 10)
-                return -1;
-            sprintf(leaf_path, "%s/leaf.jpg", resource_path);
+            sprintf(leaf_path, "leaf.jpg");
 
             /* well, let's demonstrate how to send the hard way */
 
@@ -446,7 +384,7 @@ int callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user,
             in = (char *)in + 11;
             strcpy(buf, "/usr/share/cgit");
         } else
-            strcpy(buf, resource_path);
+            strcpy(buf, "");
 
         if (strcmp(in, "/")) {
             if (*((const char *)in) != '/')
@@ -506,6 +444,8 @@ int callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user,
         break;
 
     case LWS_CALLBACK_HTTP_BODY:
+        printf("LWS_CALLBACK_HTTP_BODY\n");
+
         strncpy(buf, in, 20);
         buf[20] = '\0';
         if (len < 20)
@@ -517,15 +457,21 @@ int callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user,
         break;
 
     case LWS_CALLBACK_HTTP_BODY_COMPLETION:
+        printf("LWS_CALLBACK_HTTP_BODY_COMPLETION\n");
+        
         lwsl_notice("LWS_CALLBACK_HTTP_BODY_COMPLETION\n");
         /* the whole of the sent body arrived, close or reuse the connection */
         lws_return_http_status(wsi, HTTP_STATUS_OK, NULL);
         goto try_to_reuse;
 
     case LWS_CALLBACK_HTTP_FILE_COMPLETION:
+        printf("LWS_CALLBACK_HTTP_FILE_COMPLETION\n");
+        
         goto try_to_reuse;
 
     case LWS_CALLBACK_HTTP_WRITEABLE:
+        printf("LWS_CALLBACK_HTTP_WRITEABLE\n");
+        
         lwsl_info("LWS_CALLBACK_HTTP_WRITEABLE\n");
 
         if (pss->client_finished)
@@ -533,37 +479,6 @@ int callback_http(struct lws *wsi, enum lws_callback_reasons reason, void *user,
 
         if (pss->fd == LWS_INVALID_FILE)
             goto try_to_reuse;
-#ifdef LWS_WITH_CGI
-        if (pss->reason_bf & 1) {
-            if (lws_cgi_write_split_stdout_headers(wsi) < 0)
-                goto bail;
-
-            pss->reason_bf &= ~1;
-            break;
-        }
-#endif
-#ifndef LWS_NO_CLIENT
-        if (pss->reason_bf & 2) {
-            char *px = buf + LWS_PRE;
-            int lenx = sizeof(buf) - LWS_PRE;
-            /*
-             * our sink is writeable and our source has something
-             * to read.  So read a lump of source material of
-             * suitable size to send or what's available, whichever
-             * is the smaller.
-             */
-            pss->reason_bf &= ~2;
-            wsi1 = lws_get_child(wsi);
-            if (!wsi1)
-                break;
-            if (lws_http_client_read(wsi1, &px, &lenx) < 0)
-                goto bail;
-
-            if (pss->client_finished)
-                return -1;
-            break;
-        }
-#endif
         /*
          * we can send more of whatever it is we were sending
          */
@@ -625,7 +540,7 @@ bail:
         lws_plat_file_close(wsi, pss->fd);
 
         return -1;
-
+#if 0
     /*
      * callback for confirming to continue with client IP appear in
      * protocol 0 callback since no websocket protocol has been agreed
@@ -634,131 +549,11 @@ bail:
      * connection continue.
      */
     case LWS_CALLBACK_FILTER_NETWORK_CONNECTION:
+        printf("LWS_CALLBACK_FILTER_NETWORK_CONNECTION\n");
+        
         /* if we returned non-zero from here, we kill the connection */
         break;
 
-#ifndef LWS_NO_CLIENT
-    case LWS_CALLBACK_ESTABLISHED_CLIENT_HTTP: {
-        char ctype[64], ctlen = 0;
-        lwsl_err("LWS_CALLBACK_ESTABLISHED_CLIENT_HTTP\n");
-        p = buffer + LWS_PRE;
-        end = p + sizeof(buffer) - LWS_PRE;
-        if (lws_add_http_header_status(lws_get_parent(wsi), 200, &p, end))
-            return 1;
-        if (lws_add_http_header_by_token(lws_get_parent(wsi),
-                WSI_TOKEN_HTTP_SERVER,
-                    (unsigned char *)"libwebsockets",
-                13, &p, end))
-            return 1;
-
-        ctlen = lws_hdr_copy(wsi, ctype, sizeof(ctype), WSI_TOKEN_HTTP_CONTENT_TYPE);
-        if (ctlen > 0) {
-            if (lws_add_http_header_by_token(lws_get_parent(wsi),
-                WSI_TOKEN_HTTP_CONTENT_TYPE,
-                (unsigned char *)ctype, ctlen, &p, end))
-                return 1;
-        }
-#if 0
-        if (lws_add_http_header_content_length(lws_get_parent(wsi),
-                               file_len, &p, end))
-            return 1;
-#endif
-        if (lws_finalize_http_header(lws_get_parent(wsi), &p, end))
-            return 1;
-
-        *p = '\0';
-        lwsl_info("%s\n", buffer + LWS_PRE);
-
-        n = lws_write(lws_get_parent(wsi), buffer + LWS_PRE,
-                  p - (buffer + LWS_PRE),
-                  LWS_WRITE_HTTP_HEADERS);
-        if (n < 0)
-            return -1;
-
-        break; }
-    case LWS_CALLBACK_CLOSED_CLIENT_HTTP:
-        //lwsl_err("LWS_CALLBACK_CLOSED_CLIENT_HTTP\n");
-        return -1;
-        break;
-    case LWS_CALLBACK_RECEIVE_CLIENT_HTTP:
-        //lwsl_err("LWS_CALLBACK_RECEIVE_CLIENT_HTTP: wsi %p\n", wsi);
-        assert(lws_get_parent(wsi));
-        if (!lws_get_parent(wsi))
-            break;
-        // lwsl_err("LWS_CALLBACK_RECEIVE_CLIENT_HTTP: wsi %p: sock: %d, parent_wsi: %p, parent_sock:%d,  len %d\n",
-        //      wsi, lws_get_socket_fd(wsi),
-        //      lws_get_parent(wsi),
-        //      lws_get_socket_fd(lws_get_parent(wsi)), len);
-        pss1 = lws_wsi_user(lws_get_parent(wsi));
-        pss1->reason_bf |= 2;
-        lws_callback_on_writable(lws_get_parent(wsi));
-        break;
-    case LWS_CALLBACK_RECEIVE_CLIENT_HTTP_READ:
-        //lwsl_err("LWS_CALLBACK_RECEIVE_CLIENT_HTTP_READ len %d\n", len);
-        assert(lws_get_parent(wsi));
-        m = lws_write(lws_get_parent(wsi), (unsigned char *)in,
-                len, LWS_WRITE_HTTP);
-        if (m < 0)
-            return -1;
-        break;
-    case LWS_CALLBACK_COMPLETED_CLIENT_HTTP:
-        //lwsl_err("LWS_CALLBACK_COMPLETED_CLIENT_HTTP\n");
-        assert(lws_get_parent(wsi));
-        if (!lws_get_parent(wsi))
-            break;
-        pss1 = lws_wsi_user(lws_get_parent(wsi));
-        pss1->client_finished = 1;
-        break;
-#endif
-
-#ifdef LWS_WITH_CGI
-    /* CGI IO events (POLLIN/OUT) appear here our demo user code policy is
-     *
-     *  - POST data goes on subprocess stdin
-     *  - subprocess stdout goes on http via writeable callback
-     *  - subprocess stderr goes to the logs
-     */
-    case LWS_CALLBACK_CGI:
-        pss->args = *((struct lws_cgi_args *)in);
-        //lwsl_notice("LWS_CALLBACK_CGI: ch %d\n", pss->args.ch);
-        switch (pss->args.ch) { /* which of stdin/out/err ? */
-        case LWS_STDIN:
-            /* TBD stdin rx flow control */
-            break;
-        case LWS_STDOUT:
-            pss->reason_bf |= 1;
-            /* when writing to MASTER would not block */
-            lws_callback_on_writable(wsi);
-            break;
-        case LWS_STDERR:
-            n = read(lws_get_socket_fd(pss->args.stdwsi[LWS_STDERR]),
-                    buf, 127);
-            //lwsl_notice("stderr reads %d\n", n);
-            if (n > 0) {
-                if (buf[n - 1] != '\n')
-                    buf[n++] = '\n';
-                buf[n] = '\0';
-                lwsl_notice("CGI-stderr: %s\n", buf);
-            }
-            break;
-        }
-        break;
-
-    case LWS_CALLBACK_CGI_TERMINATED:
-        //lwsl_notice("LWS_CALLBACK_CGI_TERMINATED\n");
-        /* because we sent on openended http, close the connection */
-        return -1;
-
-    case LWS_CALLBACK_CGI_STDIN_DATA:  /* POST body for stdin */
-        //lwsl_notice("LWS_CALLBACK_CGI_STDIN_DATA\n");
-        pss->args = *((struct lws_cgi_args *)in);
-        n = write(lws_get_socket_fd(pss->args.stdwsi[LWS_STDIN]),
-              pss->args.data, pss->args.len);
-        //lwsl_notice("LWS_CALLBACK_CGI_STDIN_DATA: write says %d", n);
-        if (n < pss->args.len)
-            lwsl_notice("LWS_CALLBACK_CGI_STDIN_DATA: sent %d only %d went",
-                    n, pss->args.len);
-        return n;
 #endif
 
     /*
@@ -767,6 +562,8 @@ bail:
      */
 
     case LWS_CALLBACK_LOCK_POLL:
+        printf("LWS_CALLBACK_LOCK_POLL\n");
+        
         /*
          * lock mutex to protect pollfd state
          * called before any other POLL related callback
@@ -776,6 +573,8 @@ bail:
         break;
 
     case LWS_CALLBACK_UNLOCK_POLL:
+        printf("LWS_CALLBACK_UNLOCK_POLL\n");
+        
         /*
          * unlock mutex to protect pollfd state when
          * called after any other POLL related callback
@@ -784,35 +583,10 @@ bail:
         test_server_unlock(len);
         break;
 
-#ifdef EXTERNAL_POLL
-    case LWS_CALLBACK_ADD_POLL_FD:
-
-        if (count_pollfds >= max_poll_elements) {
-            lwsl_err("LWS_CALLBACK_ADD_POLL_FD: too many sockets to track\n");
-            return 1;
-        }
-
-        fd_lookup[pa->fd] = count_pollfds;
-        pollfds[count_pollfds].fd = pa->fd;
-        pollfds[count_pollfds].events = pa->events;
-        pollfds[count_pollfds++].revents = 0;
-        break;
-
-    case LWS_CALLBACK_DEL_POLL_FD:
-        if (!--count_pollfds)
-            break;
-        m = fd_lookup[pa->fd];
-        /* have the last guy take up the vacant slot */
-        pollfds[m] = pollfds[count_pollfds];
-        fd_lookup[pollfds[count_pollfds].fd] = m;
-        break;
-
-    case LWS_CALLBACK_CHANGE_MODE_POLL_FD:
-            pollfds[fd_lookup[pa->fd]].events = pa->events;
-        break;
-#endif
 
     case LWS_CALLBACK_GET_THREAD_ID:
+        printf("LWS_CALLBACK_GET_THREAD_ID\n");
+        
         /*
          * if you will call "lws_callback_on_writable"
          * from a different thread, return the caller thread ID
@@ -824,7 +598,37 @@ bail:
 
         break;
 
+
+    case LWS_CALLBACK_SERVER_NEW_CLIENT_INSTANTIATED:
+        printf("LWS_CALLBACK_SERVER_NEW_CLIENT_INSTANTIATED\n");
+        break;
+
+    case LWS_CALLBACK_WSI_CREATE:
+        printf("LWS_CALLBACK_WSI_CREATE\n");
+        break;
+
+    case LWS_CALLBACK_ADD_POLL_FD:
+        printf("LWS_CALLBACK_ADD_POLL_FD\n");
+        break;
+
+    case LWS_CALLBACK_CHANGE_MODE_POLL_FD:
+        printf("LWS_CALLBACK_CHANGE_MODE_POLL_FD\n");
+        break;
+
+    case LWS_CALLBACK_CONFIRM_EXTENSION_OKAY:
+        printf("LWS_CALLBACK_CONFIRM_EXTENSION_OKAY\n");
+        break;
+
+    case LWS_CALLBACK_ESTABLISHED:
+        printf("LWS_CALLBACK_ESTABLISHED\n");
+        break;
+
+    case LWS_CALLBACK_RECEIVE:
+        printf("LWS_CALLBACK_RECEIVE\n");
+        break;
+
     default:
+        printf("default reason %d\n", reason);
         break;
     }
 
@@ -834,7 +638,7 @@ bail:
 try_to_reuse:
     if (lws_http_transaction_completed(wsi))
         return -1;
-#endif
+
     return 0;
 }
 
