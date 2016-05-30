d59d475e13e88ce84b0ec5b7351ec9c97916980d
Steve Tickle <Steve@ZooKoo.com>
2016-05-30 22:53:38 +0100

    -

--- TestBench/PythonMain.c
+++ TestBench/PythonMain.c
@@ -5,6 +5,42 @@
 #include <time.h>
 #include <python3.5/Python.h>
 #include <python3.5/frameobject.h>
+#include <gtk/gtk.h>
+
+
+
+//
+//
+//
+int gmain(int argc, char *argv[])
+{
+    GtkBuilder      *builder; 
+    GtkWidget       *window;
+
+    gtk_init(&argc, &argv);
+
+    builder = gtk_builder_new();
+    gtk_builder_add_from_file (builder, "TestBenchGUI.glade", NULL);
+
+    window = GTK_WIDGET(gtk_builder_get_object(builder, "applicationwindow1"));
+    gtk_builder_connect_signals(builder, NULL);
+
+    g_object_unref(builder);
+
+    gtk_widget_show(window);                
+    gtk_main();
+
+    return 0;
+}
+
+
+//
+// called when window is closed
+//
+void on_window_main_destroy()
+{
+    gtk_main_quit();
+}
 
 
 //
@@ -97,6 +133,11 @@ int main(int argc, char* argv[])
     //
     //
     PyEval_SetTrace( &TraceFunc, NULL );
+
+    //
+    //
+    //
+    gmain(argc, argv);
 
     //
     //
