0f4344fbcd5c86b2b01344e04c90f79ff3352d4d
Steve Tickle <Steve@ZooKoo.com>
2016-06-02 21:52:44 +0100

    -

--- TestBench/PythonMain.c
+++ TestBench/PythonMain.c
@@ -21,7 +21,15 @@
 #include <time.h>
 #include <pthread.h>
 #include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h> 
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
+
+
+#define BUFSIZE 1024
 
 
 static volatile int keepRunning = 1;
@@ -150,6 +158,19 @@ void UseTBDB()
 
 
 
+//
+//
+//
+/*
+ * error - wrapper for perror
+ */
+void error(char *msg) 
+{
+  perror(msg);
+  exit(1);
+}
+
+
 
 pthread_t   serverThreadID  = {0};
 
@@ -159,6 +180,126 @@ pthread_t   serverThreadID  = {0};
 //
 void* doSomeThing(void *arg)
 {
+    int                 parentfd; /* parent socket */
+    int                 childfd; /* child socket */
+    int                 portno; /* port to listen on */
+    int                 clientlen; /* byte size of client's address */
+    struct sockaddr_in  serveraddr; /* server's addr */
+    struct sockaddr_in  clientaddr; /* client addr */
+    struct hostent*     hostp; /* client host info */
+    char                buf[BUFSIZE]; /* message buffer */
+    char*               hostaddrp; /* dotted decimal host addr string */
+    int                 optval; /* flag value for setsockopt */
+    int                 n; /* message byte size */
+
+    portno = 1234;
+
+    /* 
+    * socket: create the parent socket 
+    */
+    parentfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (parentfd < 0) 
+    {
+        error("ERROR opening socket");
+    }
+
+    /* setsockopt: Handy debugging trick that lets 
+    * us rerun the server immediately after we kill it; 
+    * otherwise we have to wait about 20 secs. 
+    * Eliminates "ERROR on binding: Address already in use" error. 
+    */
+    optval = 1;
+    setsockopt(parentfd, SOL_SOCKET, SO_REUSEADDR, (const void *)&optval , sizeof(int));
+
+    /*
+    * build the server's Internet address
+    */
+    bzero((char *) &serveraddr, sizeof(serveraddr));
+
+    /* this is an Internet address */
+    serveraddr.sin_family = AF_INET;
+
+    /* let the system figure out our IP address */
+    serveraddr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    /* this is the port we will listen on */
+    serveraddr.sin_port = htons((unsigned short)portno);
+
+    /* 
+    * bind: associate the parent socket with a port 
+    */
+    if (bind(parentfd, (struct sockaddr *) &serveraddr, sizeof(serveraddr)) < 0) 
+    {
+        error("ERROR on binding");        
+    }
+
+    /* 
+    * listen: make this socket ready to accept connection requests 
+    */
+    if (listen(parentfd, 5) < 0) /* allow 5 requests to queue up */ 
+    {
+        error("ERROR on listen");
+    }
+
+    /* 
+    * main loop: wait for a connection request, echo input line, 
+    * then close connection.
+    */
+    clientlen = sizeof(clientaddr);
+    while(true)
+    {
+        /* 
+        * accept: wait for a connection request 
+        */
+        childfd = accept(parentfd, (struct sockaddr *) &clientaddr, &clientlen);
+        if (childfd < 0) 
+        {
+          error("ERROR on accept");
+        }
+
+        /* 
+        * gethostbyaddr: determine who sent the message 
+        */
+        hostp = gethostbyaddr((const char *)&clientaddr.sin_addr.s_addr, sizeof(clientaddr.sin_addr.s_addr), AF_INET);
+        if (hostp == NULL)
+        {
+            error("ERROR on gethostbyaddr");
+        }
+        hostaddrp = inet_ntoa(clientaddr.sin_addr);
+        if (hostaddrp == NULL)
+        {
+            error("ERROR on inet_ntoa\n");
+        }
+        printf("server established connection with %s (%s)\n", 
+        hostp->h_name, hostaddrp);
+
+        /* 
+        * read: read input string from the client
+        */
+        bzero(buf, BUFSIZE);
+        n = read(childfd, buf, BUFSIZE);
+        if (n < 0) 
+        {
+            error("ERROR reading from socket");
+        }
+        printf("server received %d bytes: %s", n, buf);
+
+        /* 
+        * write: echo the input string back to the client 
+        */
+        n = write(childfd, buf, strlen(buf));
+        if (n < 0) 
+        {
+            error("ERROR writing to socket");
+        }
+
+        close(childfd);
+    }
+
+
+
+
+
     while(true)
     {
         //MilliSleep(1000);
