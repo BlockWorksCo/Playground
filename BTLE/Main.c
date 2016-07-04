aaa08041ac81a26292945a8a910d06acbcde02d1
Steve Tickle <Steve@ZooKoo.com>
2016-07-05 00:00:05 +0100

    -

--- BTLE/Main.c
+++ BTLE/Main.c
@@ -1,257 +1,225 @@
+/* Copyright (c) 2014 Nordic Semiconductor. All Rights Reserved.
+ *
+ * The information contained herein is property of Nordic Semiconductor ASA.
+ * Terms and conditions of usage are described in detail in NORDIC
+ * SEMICONDUCTOR STANDARD SOFTWARE LICENSE AGREEMENT.
+ *
+ * Licensees are granted free, non-transferable use of the information. NO
+ * WARRANTY of ANY KIND is provided. This heading must NOT be removed from
+ * the file.
+ *
+ */
 
+/** @file
+ *
+ * @defgroup ble_sdk_app_beacon_main main.c
+ * @{
+ * @ingroup ble_sdk_app_beacon
+ * @brief Beacon Transmitter Sample Application main file.
+ *
+ * This file contains the source code for an Beacon transmitter sample application.
+ */
 
-#include <stdint.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include "ble_advdata.h"
+#include "nordic_common.h"
+#include "softdevice_handler.h"
+#include "bsp.h"
+#include "app_timer.h"
 
+#define IS_SRVC_CHANGED_CHARACT_PRESENT 0                                 /**< Include or not the service_changed characteristic. if not enabled, the server's database cannot be changed for the lifetime of the device*/
 
+#define APP_CFG_NON_CONN_ADV_TIMEOUT    0                                 /**< Time for which the device must be advertising in non-connectable mode (in seconds). 0 disables timeout. */
+#define NON_CONNECTABLE_ADV_INTERVAL    MSEC_TO_UNITS(100, UNIT_0_625_MS) /**< The advertising interval for non-connectable advertisement (100 ms). This value can vary between 100ms to 10.24s). */
 
+#define APP_BEACON_INFO_LENGTH          0x17                              /**< Total length of information advertised by the Beacon. */
+#define APP_ADV_DATA_LENGTH             0x15                              /**< Length of manufacturer specific data in the advertisement. */
+#define APP_DEVICE_TYPE                 0x02                              /**< 0x02 refers to Beacon. */
+#define APP_MEASURED_RSSI               0xC3                              /**< The Beacon's measured RSSI at 1 meter distance in dBm. */
+#define APP_COMPANY_IDENTIFIER          0x0059                            /**< Company identifier for Nordic Semiconductor ASA. as per www.bluetooth.org. */
+#define APP_MAJOR_VALUE                 0x01, 0x02                        /**< Major value used to identify Beacons. */
+#define APP_MINOR_VALUE                 0x03, 0x04                        /**< Minor value used to identify Beacons. */
+#define APP_BEACON_UUID                 0x01, 0x12, 0x23, 0x34, \
+                                        0x45, 0x56, 0x67, 0x78, \
+                                        0x89, 0x9a, 0xab, 0xbc, \
+                                        0xcd, 0xde, 0xef, 0xf0            /**< Proprietary UUID for Beacon. */
 
+#define DEAD_BEEF                       0xDEADBEEF                        /**< Value used as error code on stack dump, can be used to identify stack location on stack unwind. */
 
-void ResetISR(void);
-void NmiSR(void);
-void FaultISR(void);
-void IntDefaultHandler(void);
-int main();
+#define APP_TIMER_PRESCALER             0                                 /**< Value of the RTC1 PRESCALER register. */
+#define APP_TIMER_OP_QUEUE_SIZE         4                                 /**< Size of timer operation queues. */
 
+#if defined(USE_UICR_FOR_MAJ_MIN_VALUES)
+#define MAJ_VAL_OFFSET_IN_BEACON_INFO   18                                /**< Position of the MSB of the Major Value in m_beacon_info array. */
+#define UICR_ADDRESS                    0x10001080                        /**< Address of the UICR register used by this example. The major and minor versions to be encoded into the advertising data will be picked up from this location. */
+#endif
 
-static uint8_t mainStack[1024];
-
-
-
-__attribute__ ((section(".vectors"), used))
-void (* const vetorTable[])(void) =
+static ble_gap_adv_params_t m_adv_params;                                 /**< Parameters to be passed to the stack when starting advertising. */
+static uint8_t m_beacon_info[APP_BEACON_INFO_LENGTH] =                    /**< Information advertised by the Beacon. */
 {
-   (void (*)(void))((uint32_t)mainStack + sizeof(mainStack)), // Stack pointer
-   ResetISR,                               // Reset handler
-   NmiSR,                                  // The NMI handler
-   FaultISR,                               // The hard fault handler
-   IntDefaultHandler,                      // 4 The MPU fault handler
-   IntDefaultHandler,                      // 5 The bus fault handler
-   IntDefaultHandler,                      // 6 The usage fault handler
-   0,                                      // 7 Reserved
-   0,                                      // 8 Reserved
-   0,                                      // 9 Reserved
-   0,                                      // 10 Reserved
-   IntDefaultHandler,                      // 11 SVCall handler
-   IntDefaultHandler,                      // 12 Debug monitor handler
-   0,                                      // 13 Reserved
-   IntDefaultHandler,                      // 14 The PendSV handler
-   IntDefaultHandler,                      // 15 The SysTick handler
-   IntDefaultHandler,                      // 16 GPIO Port A
-   IntDefaultHandler,                      // 17 GPIO Port B
-   IntDefaultHandler,                      // 18 GPIO Port C
-   IntDefaultHandler,                      // 19 GPIO Port D
-   0,                                      // 20 none
-   IntDefaultHandler,                      // 21 UART0 Rx and Tx
-   IntDefaultHandler,                      // 22 UART1 Rx and Tx
-   IntDefaultHandler,                      // 23 SSI0 Rx and Tx
-   IntDefaultHandler,                      // 24 I2C Master and Slave
-   0,                                      // 25 Reserved
-   0,                                      // 26 Reserved
-   0,                                      // 27 Reserved
-   0,                                      // 28 Reserved
-   0,                                      // 29 Reserved
-   IntDefaultHandler,                      // 30 ADC Sequence 0
-   0,                                      // 31 Reserved
-   0,                                      // 32 Reserved
-   0,                                      // 33 Reserved
-   IntDefaultHandler,                      // 34 Watchdog timer, timer 0
-   IntDefaultHandler,                      // 35 Timer 0 subtimer A
-   IntDefaultHandler,                      // 36 Timer 0 subtimer B
-   IntDefaultHandler,                      // 37 Timer 1 subtimer A
-   IntDefaultHandler,                      // 38 Timer 1 subtimer B
-   IntDefaultHandler,                      // 39 Timer 2 subtimer A
-   IntDefaultHandler,                      // 40 Timer 2 subtimer B
-   IntDefaultHandler,                      // 41 Analog Comparator 0
-   IntDefaultHandler,                      // 42 RFCore Rx/Tx
-   IntDefaultHandler,                      // 43 RFCore Error
-   IntDefaultHandler,                      // 44 IcePick
-   IntDefaultHandler,                      // 45 FLASH Control
-   IntDefaultHandler,                      // 46 AES
-   IntDefaultHandler,                      // 47 PKA
-   IntDefaultHandler,                      // 48 Sleep Timer
-   IntDefaultHandler,                      // 49 MacTimer
-   IntDefaultHandler,                      // 50 SSI1 Rx and Tx
-   IntDefaultHandler,                      // 51 Timer 3 subtimer A
-   IntDefaultHandler,                      // 52 Timer 3 subtimer B
-   0,                                      // 53 Reserved
-   0,                                      // 54 Reserved
-   0,                                      // 55 Reserved
-   0,                                      // 56 Reserved
-   0,                                      // 57 Reserved
-   0,                                      // 58 Reserved
-   0,                                      // 59 Reserved
-   IntDefaultHandler,                      // 60 USB 2538
-   0,                                      // 61 Reserved
-   IntDefaultHandler,                      // 62 uDMA
-   IntDefaultHandler,                      // 63 uDMA Error
-   0,                                      // 64 64-155 are not in use
-   0,                                      // 65
-   0,                                      // 66
-   0,                                      // 67
-   0,                                      // 68
-   0,                                      // 69
-   0,                                      // 70
-   0,                                      // 71
-   0,                                      // 72
-   0,                                      // 73
-   0,                                      // 74
-   0,                                      // 75
-   0,                                      // 76
-   0,                                      // 77
-   0,                                      // 78
-   0,                                      // 79
-   0,                                      // 80
-   0,                                      // 81
-   0,                                      // 82
-   0,                                      // 83
-   0,                                      // 84
-   0,                                      // 85
-   0,                                      // 86
-   0,                                      // 87
-   0,                                      // 88
-   0,                                      // 89
-   0,                                      // 90
-   0,                                      // 91
-   0,                                      // 92
-   0,                                      // 93
-   0,                                      // 94
-   0,                                      // 95
-   0,                                      // 96
-   0,                                      // 97
-   0,                                      // 98
-   0,                                      // 99
-   0,                                      // 100
-   0,                                      // 101
-   0,                                      // 102
-   0,                                      // 103
-   0,                                      // 104
-   0,                                      // 105
-   0,                                      // 106
-   0,                                      // 107
-   0,                                      // 108
-   0,                                      // 109
-   0,                                      // 110
-   0,                                      // 111
-   0,                                      // 112
-   0,                                      // 113
-   0,                                      // 114
-   0,                                      // 115
-   0,                                      // 116
-   0,                                      // 117
-   0,                                      // 118
-   0,                                      // 119
-   0,                                      // 120
-   0,                                      // 121
-   0,                                      // 122
-   0,                                      // 123
-   0,                                      // 124
-   0,                                      // 125
-   0,                                      // 126
-   0,                                      // 127
-   0,                                      // 128
-   0,                                      // 129
-   0,                                      // 130
-   0,                                      // 131
-   0,                                      // 132
-   0,                                      // 133
-   0,                                      // 134
-   0,                                      // 135
-   0,                                      // 136
-   0,                                      // 137
-   0,                                      // 138
-   0,                                      // 139
-   0,                                      // 140
-   0,                                      // 141
-   0,                                      // 142
-   0,                                      // 143
-   0,                                      // 144
-   0,                                      // 145
-   0,                                      // 146
-   0,                                      // 147
-   0,                                      // 148
-   0,                                      // 149
-   0,                                      // 150
-   0,                                      // 151
-   0,                                      // 152
-   0,                                      // 153
-   0,                                      // 154
-   0,                                      // 155
-   IntDefaultHandler,                      // 156 USB
-   IntDefaultHandler,                      // 157 RFCORE RX/TX
-   IntDefaultHandler,                      // 158 RFCORE Error
-   IntDefaultHandler,                      // 159 AES
-   IntDefaultHandler,                      // 160 PKA
-   IntDefaultHandler,                      // 161 SMTimer
-   IntDefaultHandler,                      // 162 MACTimer
+    APP_DEVICE_TYPE,     // Manufacturer specific information. Specifies the device type in this
+                         // implementation.
+    APP_ADV_DATA_LENGTH, // Manufacturer specific information. Specifies the length of the
+                         // manufacturer specific data in this implementation.
+    APP_BEACON_UUID,     // 128 bit UUID value.
+    APP_MAJOR_VALUE,     // Major arbitrary value that can be used to distinguish between Beacons.
+    APP_MINOR_VALUE,     // Minor arbitrary value that can be used to distinguish between Beacons.
+    APP_MEASURED_RSSI    // Manufacturer specific information. The Beacon's measured TX power in
+                         // this implementation.
 };
 
-
-
-
-
-//
-//
-//
-extern uint32_t _etext;
-extern uint32_t _data;
-extern uint32_t _edata;
-extern uint32_t _bss;
-extern uint32_t _ebss;
-
-
-
-
-//
-// And here are the weak interrupt handlers.
-//
-void NmiSR (void)
+/**@brief Callback function for asserts in the SoftDevice.
+ *
+ * @details This function will be called in case of an assert in the SoftDevice.
+ *
+ * @warning This handler is an example only and does not fit a final product. You need to analyze
+ *          how your product is supposed to react in case of Assert.
+ * @warning On assert from the SoftDevice, the system can only recover on reset.
+ *
+ * @param[in]   line_num   Line number of the failing ASSERT call.
+ * @param[in]   file_name  File name of the failing ASSERT call.
+ */
+void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
 {
-    ResetISR();
+    app_error_handler(DEAD_BEEF, line_num, p_file_name);
+}
 
-    while(true);
+/**@brief Function for initializing the Advertising functionality.
+ *
+ * @details Encodes the required advertising data and passes it to the stack.
+ *          Also builds a structure to be passed to the stack when starting advertising.
+ */
+static void advertising_init(void)
+{
+    uint32_t      err_code;
+    ble_advdata_t advdata;
+    uint8_t       flags = BLE_GAP_ADV_FLAG_BR_EDR_NOT_SUPPORTED;
+
+    ble_advdata_manuf_data_t manuf_specific_data;
+
+    manuf_specific_data.company_identifier = APP_COMPANY_IDENTIFIER;
+
+#if defined(USE_UICR_FOR_MAJ_MIN_VALUES)
+    // If USE_UICR_FOR_MAJ_MIN_VALUES is defined, the major and minor values will be read from the
+    // UICR instead of using the default values. The major and minor values obtained from the UICR
+    // are encoded into advertising data in big endian order (MSB First).
+    // To set the UICR used by this example to a desired value, write to the address 0x10001080
+    // using the nrfjprog tool. The command to be used is as follows.
+    // nrfjprog --snr <Segger-chip-Serial-Number> --memwr 0x10001080 --val <your major/minor value>
+    // For example, for a major value and minor value of 0xabcd and 0x0102 respectively, the
+    // the following command should be used.
+    // nrfjprog --snr <Segger-chip-Serial-Number> --memwr 0x10001080 --val 0xabcd0102
+    uint16_t major_value = ((*(uint32_t *)UICR_ADDRESS) & 0xFFFF0000) >> 16;
+    uint16_t minor_value = ((*(uint32_t *)UICR_ADDRESS) & 0x0000FFFF);
+
+    uint8_t index = MAJ_VAL_OFFSET_IN_BEACON_INFO;
+
+    m_beacon_info[index++] = MSB_16(major_value);
+    m_beacon_info[index++] = LSB_16(major_value);
+
+    m_beacon_info[index++] = MSB_16(minor_value);
+    m_beacon_info[index++] = LSB_16(minor_value);
+#endif
+
+    manuf_specific_data.data.p_data = (uint8_t *) m_beacon_info;
+    manuf_specific_data.data.size   = APP_BEACON_INFO_LENGTH;
+
+    // Build and set advertising data.
+    memset(&advdata, 0, sizeof(advdata));
+
+    advdata.name_type             = BLE_ADVDATA_NO_NAME;
+    advdata.flags                 = flags;
+    advdata.p_manuf_specific_data = &manuf_specific_data;
+
+    err_code = ble_advdata_set(&advdata, NULL);
+    APP_ERROR_CHECK(err_code);
+
+    // Initialize advertising parameters (used when starting advertising).
+    memset(&m_adv_params, 0, sizeof(m_adv_params));
+
+    m_adv_params.type        = BLE_GAP_ADV_TYPE_ADV_NONCONN_IND;
+    m_adv_params.p_peer_addr = NULL;                             // Undirected advertisement.
+    m_adv_params.fp          = BLE_GAP_ADV_FP_ANY;
+    m_adv_params.interval    = NON_CONNECTABLE_ADV_INTERVAL;
+    m_adv_params.timeout     = APP_CFG_NON_CONN_ADV_TIMEOUT;
 }
 
 
-//
-//
-//
-void FaultISR (void)
+/**@brief Function for starting advertising.
+ */
+static void advertising_start(void)
 {
-    while(true);
+    uint32_t err_code;
+
+    err_code = sd_ble_gap_adv_start(&m_adv_params);
+    APP_ERROR_CHECK(err_code);
+
+    err_code = bsp_indication_set(BSP_INDICATE_ADVERTISING);
+    APP_ERROR_CHECK(err_code);
 }
 
 
-void IntDefaultHandler (void)
+/**@brief Function for initializing the BLE stack.
+ *
+ * @details Initializes the SoftDevice and the BLE event interrupt.
+ */
+static void ble_stack_init(void)
 {
-    while(true);
+    // Initialize the SoftDevice handler module.
+    SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_XTAL_20_PPM, NULL);
+
+    // Enable BLE stack
+    uint32_t err_code;
+    ble_enable_params_t ble_enable_params;
+    memset(&ble_enable_params, 0, sizeof(ble_enable_params));
+#if (defined(S130) && defined(S132))
+    ble_enable_params.gatts_enable_params.attr_tab_size   = BLE_GATTS_ATTR_TAB_SIZE_DEFAULT;
+#endif
+    ble_enable_params.gatts_enable_params.service_changed = IS_SRVC_CHANGED_CHARACT_PRESENT;
+    err_code = sd_ble_enable(&ble_enable_params);
+    APP_ERROR_CHECK(err_code);
 }
 
 
-//
-//
-//
-void ResetISR (void)
+/**@brief Function for doing power management.
+ */
+static void power_manage(void)
 {
-   //
-   // Call the application's entry point.
-   //
-   main();
-
-   //
-   // End here if return from main()
-   //
-   while(true);
+    uint32_t err_code = sd_app_evt_wait();
+    APP_ERROR_CHECK(err_code);
 }
 
 
-
-int main()
+/**
+ * @brief Function for application main entry.
+ */
+int main(void)
 {
-    volatile uint32_t   i   = 0;
+    uint32_t err_code;
+    // Initialize.
+    APP_TIMER_INIT(APP_TIMER_PRESCALER, APP_TIMER_OP_QUEUE_SIZE, false);
+    err_code = bsp_init(BSP_INIT_LED, APP_TIMER_TICKS(100, APP_TIMER_PRESCALER), NULL);
+    APP_ERROR_CHECK(err_code);
+    ble_stack_init();
+    advertising_init();
 
-    while(true)
+    // Start execution.
+    advertising_start();
+
+    // Enter main loop.
+    for (;; )
     {
-        i++;
+        power_manage();
     }
 }
+
+
+/**
+ * @}
+ */
+
+
+
+
+ 
