1fde8481eb36a1a11dd9a430ea0fb68ac9dae4c8
Steve Tickle <Steve@ZooKoo.com>
2016-07-15 23:03:52 +0100

    -

--- BTLE/AirUI/app/src/main/java/com/example/blockworks/airui/DeviceUIActivity.java
+++ BTLE/AirUI/app/src/main/java/com/example/blockworks/airui/DeviceUIActivity.java
@@ -18,7 +18,8 @@ import android.webkit.WebView;
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  */
-public class DeviceUIActivity extends Activity {
+public class DeviceUIActivity extends Activity
+{
     /**
      * Whether or not the system UI should be auto-hidden after
      * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
@@ -36,61 +37,78 @@ public class DeviceUIActivity extends Activity {
      * and a change of the status and navigation bar.
      */
     private static final int UI_ANIMATION_DELAY = 300;
+
     private final Handler mHideHandler = new Handler();
-    //private View mContentView;
-    private final Runnable mHidePart2Runnable = new Runnable() {
+
+    private final Runnable mHidePart2Runnable = new Runnable()
+    {
         @SuppressLint("InlinedApi")
         @Override
-        public void run() {
-            // Delayed removal of status and navigation bar
-
-            // Note that some of these constants are new as of API 16 (Jelly Bean)
-            // and API 19 (KitKat). It is safe to use them, as they are inlined
-            // at compile-time and do nothing on earlier devices.
-            /*mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                    | View.SYSTEM_UI_FLAG_FULLSCREEN
-                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                    | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                    | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                    | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);*/
+        public void run()
+        {
         }
     };
+
+
+    //
     //private View mControlsView;
-    private final Runnable mShowPart2Runnable = new Runnable() {
+    //
+    private final Runnable mShowPart2Runnable = new Runnable()
+    {
         @Override
-        public void run() {
+        public void run()
+        {
             // Delayed display of UI elements
             ActionBar actionBar = getActionBar();
-            if (actionBar != null) {
+            if (actionBar != null)
+            {
                 actionBar.show();
             }
-            //mControlsView.setVisibility(View.VISIBLE);
         }
     };
+
+
+
+    //
+    //
+    //
     private boolean mVisible;
-    private final Runnable mHideRunnable = new Runnable() {
+    private final Runnable mHideRunnable = new Runnable()
+    {
         @Override
-        public void run() {
+        public void run()
+        {
             hide();
         }
     };
+
+
+
     /**
      * Touch listener to use for in-layout UI controls to delay hiding the
      * system UI. This is to prevent the jarring behavior of controls going away
      * while interacting with activity UI.
      */
-    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener() {
+    private final View.OnTouchListener mDelayHideTouchListener = new View.OnTouchListener()
+    {
         @Override
-        public boolean onTouch(View view, MotionEvent motionEvent) {
-            if (AUTO_HIDE) {
+        public boolean onTouch(View view, MotionEvent motionEvent)
+        {
+            if (AUTO_HIDE)
+            {
                 delayedHide(AUTO_HIDE_DELAY_MILLIS);
             }
             return false;
         }
     };
 
+
+    //
+    //
+    //
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_device_ui);
@@ -115,23 +133,15 @@ public class DeviceUIActivity extends Activity {
         // Show the webview with the UI on it.
         //
         webView.loadUrl(url);
-
-        // Set up the user interaction to manually show or hide the system UI.
-        /*mContentView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                toggle();
-            }
-        });*/
-
-        // Upon interacting with UI controls, delay any scheduled hide()
-        // operations to prevent the jarring behavior of controls going away
-        // while interacting with the UI.
-        //findViewById(R.id.dummy_button).setOnTouchListener(mDelayHideTouchListener);
     }
 
+
+    //
+    //
+    //
     @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
+    protected void onPostCreate(Bundle savedInstanceState)
+    {
         super.onPostCreate(savedInstanceState);
 
         // Trigger the initial hide() shortly after the activity has been
@@ -140,21 +150,19 @@ public class DeviceUIActivity extends Activity {
         delayedHide(100);
     }
 
-    private void toggle() {
-        if (mVisible) {
-            hide();
-        } else {
-            show();
-        }
-    }
 
-    private void hide() {
+
+    //
+    //
+    //
+    private void hide()
+    {
         // Hide UI first
         ActionBar actionBar = getActionBar();
-        if (actionBar != null) {
+        if (actionBar != null)
+        {
             actionBar.hide();
         }
-        //mControlsView.setVisibility(View.GONE);
         mVisible = false;
 
         // Schedule a runnable to remove the status and navigation bar after a delay
@@ -162,11 +170,13 @@ public class DeviceUIActivity extends Activity {
         mHideHandler.postDelayed(mHidePart2Runnable, UI_ANIMATION_DELAY);
     }
 
+
+    //
+    //
+    //
     @SuppressLint("InlinedApi")
-    private void show() {
-        // Show the system bar
-        /*mContentView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION);*/
+    private void show()
+    {
         mVisible = true;
 
         // Schedule a runnable to display UI elements after a delay
@@ -178,7 +188,8 @@ public class DeviceUIActivity extends Activity {
      * Schedules a call to hide() in [delay] milliseconds, canceling any
      * previously scheduled calls.
      */
-    private void delayedHide(int delayMillis) {
+    private void delayedHide(int delayMillis)
+    {
         mHideHandler.removeCallbacks(mHideRunnable);
         mHideHandler.postDelayed(mHideRunnable, delayMillis);
     }
