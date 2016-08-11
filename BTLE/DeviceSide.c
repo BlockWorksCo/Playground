0b780cac25edfdce43703b66082a98b99de3cd28
Steve Tickle <Steve@ZooKoo.com>
2016-08-11 08:05:22 +0100

    -

--- BTLE/DeviceSide.c
+++ BTLE/DeviceSide.c
@@ -8,10 +8,138 @@
 
 #include <sys/types.h>
 #include <sys/stat.h>
+#include <sys/ioctl.h>
 #include <fcntl.h>
 #include <unistd.h>
 
 
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+//
+//
+//
+void InternalFailure()
+{
+    printf("<InternalFailure>\n");
+    exit(-1);
+}
+
+
+//
+//
+//
+void ValidatePointerForRW( void* pointer )
+{
+    if(pointer == NULL)
+    {
+        InternalFailure();
+    }
+}
+
+
+
+int     fd      = -1;
+
+//
+//
+//
+typedef struct
+{
+    uint32_t    timestamp;
+    uint32_t    eventType;
+    uint32_t    numberOfPayloadBytes;
+    uint8_t     payload[1];
+
+} Event;
+
+typedef void (*EventNotificationHandler)(Event*);
+
+
+
+//
+//
+//
+void TransmitEvent( Event* event )
+{
+    write( fd, event, sizeof(*event) );
+}
+
+
+//
+//
+//
+void RequestEventNotification( EventNotificationHandler handler )
+{
+
+}
+
+//
+//
+//
+bool PollForEvent( Event* event )
+{
+    bool    success                 = false;
+    int     numberOfBytesAvailable  = 0;
+    int     ioctlResult             = ioctl( fd, FIONREAD, &numberOfBytesAvailable );
+
+    ValidatePointerForRW(event);
+
+    if( ioctlResult != 0 )
+    {
+        //
+        // ioctl failed on fd.
+        //
+        InternalFailure();
+    }
+    else
+    {
+        if(numberOfBytesAvailable > sizeof(Event) )
+        {
+            ssize_t bytesRead  = read( fd, event, sizeof(*event) );
+            if( bytesRead == sizeof(*event) )
+            {
+                success     = true;
+            }
+            else
+            {
+                //
+                // Couldn't read advertised number of bytes.
+                //
+                InternalFailure();
+            }
+        }
+    }
+
+    return success;
+}
+
+
+//
+//
+//
+void AuraInitialise()
+{
+    fd  = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY );
+    if(fd == -1)
+    {
+        //
+        // Couldn't open serial port.
+        //
+        InternalFailure();
+    }
+
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+void EventHandler(Event* event)
+{
+    printf( "<Event received>\n" );
+}
 
 
 //
@@ -20,39 +148,42 @@
 int main()
 {
     printf("Aura DeviceSide demo.\n");
-    int     fd  = open("/dev/ttyAMA0", O_RDWR | O_NOCTTY | O_NDELAY );
-    if(fd == -1)
-    {
-        printf("<Couldn't open UART>\n");
-        exit(-1);
-    }
-    else
-    {
-        printf("<Opened UART>\n");
 
-        while(true)
+    AuraInitialise();
+
+    RequestEventNotification( EventHandler );
+
+    //
+    //
+    //
+    while(true)
+    {
+        fd_set fds;
+        FD_ZERO(&fds);
+        FD_SET(fd, &fds);
+        struct timeval timeout = { 1, 0 }; /* 10 seconds */
+
+        int ret = select(fd+1, &fds, NULL, NULL, &timeout);
+        if (ret ==1)
         {
-            fd_set fds;
-            FD_ZERO(&fds);
-            FD_SET(fd, &fds);
-            struct timeval timeout = { 1, 0 }; /* 10 seconds */
+            Event   event;
 
-            int ret = select(fd+1, &fds, NULL, NULL, &timeout);
-            if (ret ==1)
+            if(PollForEvent( &event ) == true)
             {
-                uint8_t         rxData  = 0;
-                read( fd, &rxData, 1 );
-                printf("%c", rxData);
-            }
-            else if(ret == 0)
-            {
-                perror("timeout error ");
-            }
-            else if (ret ==-1)
-            {
-                perror("some other error");
+                printf("<EventReceived>\n");
             }
 
+            uint8_t         rxData  = 0;
+            read( fd, &rxData, 1 );
+            printf("%c", rxData);
+        }
+        else if(ret == 0)
+        {
+            perror("timeout error ");
+        }
+        else if (ret ==-1)
+        {
+            perror("some other error");
         }
 
     }
