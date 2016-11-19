bba27562b49c9ee4c627974c5499227575297ad5
Steve Tickle <Steve@ZooKoo.com>
2016-11-19 10:06:42 +0000

    -

---
+++ Burn/InputMain.c
@@ -0,0 +1,36 @@
+//
+// https://github.com/thoughtpolice/enable_arm_pmu.git
+//
+
+
+
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "armpmu_lib.h"
+
+
+
+
+int main()
+{
+	uint32_t 	start;
+	uint32_t	end;
+
+	while(true)
+	{
+		start 	= rdtsc32();
+		for(uint32_t i=0; i<1000; i++)
+		{
+			static volatile 	uint32_t i 	= 0;
+			i++;
+		}
+		end 	= rdtsc32();
+		
+		printf("diff = %d\n", end-start);
+	}
+
+}
+
+
