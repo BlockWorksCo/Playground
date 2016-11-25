50bf3eef00b9d6d222fb0babde1ebea1e8d5ed18
Steve Tickle <Steve@ZooKoo.com>
2016-11-25 23:21:55 +0000

    -

--- Burn/InputMain.c
+++ Burn/InputMain.c
@@ -28,17 +28,17 @@
 //
 typedef struct
 {
-	volatile uint32_t	CFG0;
-	volatile uint32_t	CFG1;
-	volatile uint32_t	CFG2;
-	volatile uint32_t	CFG3;
+    volatile uint32_t	CFG0;
+    volatile uint32_t	CFG1;
+    volatile uint32_t	CFG2;
+    volatile uint32_t	CFG3;
 
-	volatile uint32_t	DAT;
-	volatile uint32_t	DRV0;
-	volatile uint32_t	DRV1;
+    volatile uint32_t	DAT;
+    volatile uint32_t	DRV0;
+    volatile uint32_t	DRV1;
 
-	volatile uint32_t	PUL0;
-	volatile uint32_t	PUL1;
+    volatile uint32_t	PUL0;
+    volatile uint32_t	PUL1;
 
 } GPIOPort;
 
@@ -48,38 +48,38 @@ typedef struct
 //
 uint32_t* SetupGPIO()
 {
-	const unsigned long	GPIO_BASE 		= 0x01C20800;
-	const unsigned long 	PAGE_SIZE 		= 4096;
-	const unsigned long	GPIO_BASEPage 		= GPIO_BASE & ~(PAGE_SIZE-1);
-	uint32_t 		GPIO_BASEOffsetIntoPage	= GPIO_BASE - GPIO_BASEPage;
-  	int			mem_fd			= 0;
-  	void*			regAddrMap 		= MAP_FAILED;
+    const unsigned long	GPIO_BASE 		= 0x01C20800;
+    const unsigned long 	PAGE_SIZE 		= 4096;
+    const unsigned long	GPIO_BASEPage 		= GPIO_BASE & ~(PAGE_SIZE-1);
+    uint32_t 		GPIO_BASEOffsetIntoPage	= GPIO_BASE - GPIO_BASEPage;
+      int			mem_fd			= 0;
+      void*			regAddrMap 		= MAP_FAILED;
 
 
-	if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) 
-	{
-		perror("can't open /dev/mem");
-		exit (1);
-	}
+    if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) 
+    {
+        perror("can't open /dev/mem");
+        exit (1);
+    }
 
-  	regAddrMap = mmap(
-      		NULL,          
-      		8192,       	
-			PROT_READ|PROT_WRITE|PROT_EXEC,// Enable reading & writting to mapped memory
-			MAP_SHARED,       //Shared with other processes
-      		mem_fd,           
-		GPIO_BASEPage);
+      regAddrMap = mmap(
+              NULL,          
+              8192,       	
+            PROT_READ|PROT_WRITE|PROT_EXEC,// Enable reading & writting to mapped memory
+            MAP_SHARED,       //Shared with other processes
+              mem_fd,           
+        GPIO_BASEPage);
 
-  	if (regAddrMap == MAP_FAILED) 
-	{
-          	perror("mmap error");
-          	close(mem_fd);
-          	exit (1);
-  	}
+      if (regAddrMap == MAP_FAILED) 
+    {
+              perror("mmap error");
+              close(mem_fd);
+              exit (1);
+      }
 
-	printf("gpio mapped to %p = %08x\n", regAddrMap, (uint32_t)GPIO_BASEPage);
+    printf("gpio mapped to %p = %08x\n", regAddrMap, (uint32_t)GPIO_BASEPage);
 
-	return (uint32_t*)(regAddrMap + GPIO_BASEOffsetIntoPage);
+    return (uint32_t*)(regAddrMap + GPIO_BASEOffsetIntoPage);
 }
 
 
@@ -90,34 +90,34 @@ uint32_t* SetupGPIO()
 //
 typedef struct
 {
-	volatile uint32_t	reserved0;
+    volatile uint32_t	reserved0;
 
-	volatile uint32_t	CTL;			// 0x04 Global Control
-	volatile uint32_t	INTCTL;			// 0x08 Transfer Conrol
+    volatile uint32_t	CTL;			// 0x04 Global Control
+    volatile uint32_t	INTCTL;			// 0x08 Transfer Conrol
 
-	volatile uint32_t	reserved1;		// 0x0c
+    volatile uint32_t	reserved1;		// 0x0c
 
-	volatile uint32_t	IER;			// 0x10 Interrupt control
-	volatile uint32_t	INT_STA;		// 0x14 Interrupt status
-	volatile uint32_t	FCR;			// 0x18 FIFO control
-	volatile uint32_t	FSR;			// 0x1c FIFO status
-	volatile uint32_t	WAIT;			// 0x20 wait clock counter
-	volatile uint32_t	CCTL;			// 0x24 Clock rate control
+    volatile uint32_t	IER;			// 0x10 Interrupt control
+    volatile uint32_t	INT_STA;		// 0x14 Interrupt status
+    volatile uint32_t	FCR;			// 0x18 FIFO control
+    volatile uint32_t	FSR;			// 0x1c FIFO status
+    volatile uint32_t	WAIT;			// 0x20 wait clock counter
+    volatile uint32_t	CCTL;			// 0x24 Clock rate control
 
-	volatile uint32_t	reserved2;		// 0x28
-	volatile uint32_t	reserved3;		// 0x2c
+    volatile uint32_t	reserved2;		// 0x28
+    volatile uint32_t	reserved3;		// 0x2c
 
-	volatile uint32_t	BC;				// 0x30 burst counter
-	volatile uint32_t	TC;				// 0x3c transmit counter
-	volatile uint32_t	BCC;			//  0x38 Burst control
+    volatile uint32_t	BC;				// 0x30 burst counter
+    volatile uint32_t	TC;				// 0x3c transmit counter
+    volatile uint32_t	BCC;			//  0x38 Burst control
 
-	volatile uint8_t	reserved4[0x1c0];// 0x40
+    volatile uint8_t	reserved4[0x1c0];// 0x40
 
-	volatile uint32_t	TXD;			// 0x200
+    volatile uint32_t	TXD;			// 0x200
 
-	volatile uint8_t	reserved5[0xfc];// 0x204
+    volatile uint8_t	reserved5[0xfc];// 0x204
 
-	volatile uint32_t	RXD;			// 0x300
+    volatile uint32_t	RXD;			// 0x300
 
 } SPIPort;
 
@@ -127,44 +127,44 @@ typedef struct
 //
 uint32_t* SetupSPI()
 {
-	//const uint32_t	BASE 					= 0x01C68000;	// SPI0
-	const uint32_t 		BASE 					= 0x01C69000; 	// SPI1
-	const uint32_t  	PAGE_SIZE 				= 4096*16;
-	const uint32_t		BASEPage 				= BASE & ~(PAGE_SIZE-1);
-	uint32_t 			BASEOffsetIntoPage		= BASE - BASEPage;
-  	int					mem_fd					= 0;
-  	void*				regAddrMap 				= MAP_FAILED;
+    //const uint32_t	BASE 					= 0x01C68000;	// SPI0
+    const uint32_t 		BASE 					= 0x01C69000; 	// SPI1
+    const uint32_t  	PAGE_SIZE 				= 4096*16;
+    const uint32_t		BASEPage 				= BASE & ~(PAGE_SIZE-1);
+    uint32_t 			BASEOffsetIntoPage		= BASE - BASEPage;
+    int					mem_fd					= 0;
+    void*				regAddrMap 				= MAP_FAILED;
 
 
-	printf("BASE 						= %08x\n", BASE);
-	printf("BASEPage 					= %08x\n", BASEPage);
-	printf("BASEOffsetIntoPage 			= %08x\n", BASEOffsetIntoPage);
-	printf("BASE+BASEOffsetIntoPage 	= %08x\n", BASEPage+BASEOffsetIntoPage);
+    printf("BASE 						= %08x\n", BASE);
+    printf("BASEPage 					= %08x\n", BASEPage);
+    printf("BASEOffsetIntoPage 			= %08x\n", BASEOffsetIntoPage);
+    printf("BASE+BASEOffsetIntoPage 	= %08x\n", BASEPage+BASEOffsetIntoPage);
 
-	if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) 
-	{
-		perror("can't open /dev/mem");
-		exit (1);
-	}
+    if ((mem_fd = open("/dev/mem", O_RDWR|O_SYNC) ) < 0) 
+    {
+        perror("can't open /dev/mem");
+        exit (1);
+    }
 
-  	regAddrMap = mmap(
-      		NULL,          
-      		BASEOffsetIntoPage+PAGE_SIZE,       	
-			PROT_READ|PROT_WRITE|PROT_EXEC,// Enable reading & writting to mapped memory
-			MAP_SHARED,       //Shared with other processes
-      		mem_fd,           
-			BASEPage);
+      regAddrMap = mmap(
+              NULL,          
+              BASEOffsetIntoPage+PAGE_SIZE,       	
+            PROT_READ|PROT_WRITE|PROT_EXEC,// Enable reading & writting to mapped memory
+            MAP_SHARED,       //Shared with other processes
+              mem_fd,           
+            BASEPage);
 
-  	if (regAddrMap == MAP_FAILED) 
-	{
-          	perror("mmap error");
-          	close(mem_fd);
-          	exit (1);
-  	}
+      if (regAddrMap == MAP_FAILED) 
+    {
+              perror("mmap error");
+              close(mem_fd);
+              exit (1);
+      }
 
-	printf("spi mapped to %p = %08x (+%08x)\n", regAddrMap, (uint32_t)BASEPage, BASEOffsetIntoPage);
+    printf("spi mapped to %p = %08x (+%08x)\n", regAddrMap, (uint32_t)BASEPage, BASEOffsetIntoPage);
 
-	return (uint32_t*)(regAddrMap + BASEOffsetIntoPage);
+    return (uint32_t*)(regAddrMap + BASEOffsetIntoPage);
 }
 
 
@@ -187,71 +187,79 @@ volatile GPIOPort*	portL	= 0;
 //
 int main()
 {
-	uint32_t 			start;
-	uint32_t			end;
+    uint32_t 			start;
+    uint32_t			end;
 
-	gpio 	= (GPIOPort*)SetupGPIO();
-	spi 	= (SPIPort*)SetupSPI();
+    gpio 	= (GPIOPort*)SetupGPIO();
+    spi 	= (SPIPort*)SetupSPI();
 
-	portA	= &gpio[0];
-	portC	= &gpio[1];
-	portD	= &gpio[2];
-	portE	= &gpio[3];
-	portF	= &gpio[4];
-	portG	= &gpio[5];
-	portL	= &gpio[6];
+    portA	= &gpio[0];
+    portC	= &gpio[1];
+    portD	= &gpio[2];
+    portE	= &gpio[3];
+    portF	= &gpio[4];
+    portG	= &gpio[5];
+    portL	= &gpio[6];
 
-	//
-	// Pin setup.
-	//
-	portA->CFG1 	&= ~0xfff00000;
-	portA->CFG1 	|=  0x22200000;
+    //
+    // Pin setup.
+    //
+    portA->CFG1 	&= ~0xfff00000;
+    portA->CFG1 	|=  0x22200000;
 
-	portA->CFG2 	&= ~0x0000000f;
-	portA->CFG2 	|=  0x00000002;
+    portA->CFG2 	&= ~0x0000000f;
+    portA->CFG2 	|=  0x00000002;
 
-	//
-	// SPI port setup.
-	//
-	spi->CTL 	= 0x80000083;
+    //
+    // SPI port setup.
+    //
+    spi->CTL 	= 0x00000083;
+    spi->INTCTL = 0x000001c4;
+    spi->IER 	= 0x00000000;
+    spi->INT_STA= 0x00000032;
+    spi->FCR 	= 0x00200020;
+    spi->FSR 	= 0x00000000;
+    spi->WAIT 	= 0x00000000;
+    spi->CCTL 	= 0x00001004;
+    spi->BC 	= 0x00000000;
+    spi->TC 	= 0x00000000;
+    spi->BCC 	= 0x00000000;
+    //msync( (void*)spi, 4096, MS_SYNC|MS_INVALIDATE);
 
-	//spi->CTL 	|= 0x80000000;
-	while( (spi->CTL&0x80000000)  != 0);
 
-	spi->CCTL 	= 0x00001004;
-	spi->CTL 	= 0x00000083;
+    printf("CTL=%08x\n", spi->CTL);
+    printf("INTCTL=%08x\n", spi->INTCTL);
+    printf("IER=%08x\n", spi->IER);
+    printf("INT_STA=%08x\n", spi->INT_STA);
+    printf("FCR=%08x\n", spi->FCR);
+    printf("FSR=%08x\n", spi->FSR);
+    printf("WAIT=%08x\n", spi->WAIT);
+    printf("CCTL=%08x\n", spi->CCTL);
+    printf("BC=%08x\n", spi->BC);
+    printf("TC=%08x\n", spi->TC);
+    printf("BCC=%08x\n", spi->BCC);
 
-
-	printf("CTL=%08x\n", spi->CTL);
-	printf("INTCTL=%08x\n", spi->INTCTL);
-	printf("IER=%08x\n", spi->IER);
-	printf("INT_STA=%08x\n", spi->INT_STA);
-	printf("FCR=%08x\n", spi->FCR);
-	printf("FSR=%08x\n", spi->FSR);
-	printf("CCTL=%08x\n", spi->CCTL);
-	printf("TC=%08x\n", spi->TC);
-
-	uint32_t 	i 	= 0;
-	while(true)
-	{
-		printf("FSR=%08x\n", spi->FSR);
-		printf("INT_STA=%08x\n", spi->INT_STA);
-		spi->TXD 	= i;
-		i++;
-		sleep(1);
+    uint32_t 	i 	= 0;
+    while(true)
+    {
+        printf("FSR=%08x\n", spi->FSR);
+        printf("INT_STA=%08x\n", spi->INT_STA);
+        spi->TXD 	= i;
+        i++;
+        sleep(1);
 
 #if 0
-		start 	= rdtsc32();
-		for(uint32_t i=0; i<1000; i++)
-		{
-			static volatile 	uint32_t i 	= 0;
-			i++;
-		}
-		end 	= rdtsc32();
-		
-		printf("diff = %d\n", end-start);
+        start 	= rdtsc32();
+        for(uint32_t i=0; i<1000; i++)
+        {
+            static volatile 	uint32_t i 	= 0;
+            i++;
+        }
+        end 	= rdtsc32();
+        
+        printf("diff = %d\n", end-start);
 #endif
-	}
+    }
 
 }
 
