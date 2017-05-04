a9d872ba584c0ffbf0902564977032afe38da42b
Steve Tickle <Steve@BlockWorks.co>
2017-05-04 23:10:08 +0100

    - DLMS header parsing.

--- DLMS/Main.cpp
+++ DLMS/Main.cpp
@@ -46,6 +46,86 @@ private:
 
 
 
+//
+//
+//
+typedef struct
+{
+    uint8_t     d0;
+    uint8_t     d1;
+    uint8_t     d2;
+    uint8_t     d3;
+    uint8_t     d4;
+    uint8_t     d5;
+
+} LogicalName;
+
+
+//
+//
+//
+class DLMSParser
+{
+
+public:
+
+    DLMSParser()
+    {
+    }
+
+
+    bool ProcessLLC(uint32_t value)
+    {
+        printf("LLC: %08x\n", value);
+    }
+
+    bool ProcessRequestType(uint16_t value)
+    {
+        printf("RequestType: %04x\n", value);
+    }
+
+    bool ProcessInvokeId(uint8_t value)
+    {
+        printf("InvokeID: %02x\n", value);
+    }
+
+    bool ProcessInterfaceClass(uint16_t value)
+    {
+        printf("InterfaceClass: %04x\n", value);
+    }
+
+    bool ProcessLogicalName(LogicalName value)
+    {
+        printf("InterfaceClass: %02x.%02x.%02x.%02x.%02x.%02x\n", value.d0, value.d1, value.d2, value.d3, value.d4, value.d5);
+    }
+
+    bool ProcessAttributeNumber(uint16_t value)
+    {
+        printf("AttributeNumber: %04x\n", value);
+    }
+
+
+
+    //
+    // Called if the check-sequence passes.
+    //
+    void AbortProcessing()
+    {
+        printf("Abort\n");
+    }
+
+    void CompleteProcessing()
+    {
+        printf("Complete\n");
+    }
+
+
+private:
+
+
+};
+
+
 
 
 //
@@ -56,8 +136,9 @@ class HDLCFrame
 
 public:
 
-    HDLCFrame( ByteStream& _stream ) :
-        stream( _stream )
+    HDLCFrame( ByteStream& _stream, DLMSParser& _upperLayer ) :
+        stream( _stream ),
+        upperLayer( _upperLayer )
     {
     }
 
@@ -106,12 +187,183 @@ public:
     }
 
     
+    
+    bool Parse()
+    {
+        bool        success     = false;
+        uint32_t    position    = 0;        
+        uint8_t     header[7];
+        uint8_t     fcs[2];
+        uint8_t     llc[4];
+        uint8_t     requestType[2];
+        LogicalName logicalName;
+        uint8_t     invokeId;
+        uint8_t     classId[2];
+        uint8_t     attributeNumber[2];
+
+        do
+        {
+            uint8_t     value;
+            success     = Get_UINT8( value );
+            if( success == true )
+            {
+                //
+                // Parse the header bytes.
+                //
+                switch(position)
+                {
+                    case 0:
+                        header[0]   = value;
+                        break;
+
+                    case 1:
+                        header[1]   = value;
+                        break;
+
+                    case 2:
+                        header[2]   = value;
+                        break;
+
+                    case 3:
+                        header[3]   = value;
+                        break;
+
+                    case 4:
+                        header[4]   = value;
+                        break;
+
+                    case 5:
+                        header[5]   = value;
+                        break;
+
+                    case 6:
+                        header[6]   = value;
+                        break;
+
+                    case 7:
+                        llc[0]      = value;
+                        break;
+
+                    case 8:
+                        llc[1]      = value;
+                        break;
+
+                    case 9:
+                    {
+                        llc[2]      = value;
+                        uint32_t    field   = (llc[0]<<16) | (llc[1]<<8) | (llc[2]);
+                        upperLayer.ProcessLLC( field );
+                        break;
+                    }   
+
+                    case 10:
+                        requestType[0]      = value;
+                        break;
+
+                    case 11:
+                    {
+                        requestType[1]      = value;
+                        uint16_t    field   = (requestType[0]<<8) | (requestType[1]);
+                        upperLayer.ProcessRequestType( field );
+                        break;
+                    }
+
+                    case 12:
+                    {
+                        invokeId          = value;
+                        upperLayer.ProcessInvokeId( invokeId );
+                        break;
+                    }
+
+                    case 13:
+                        classId[0]      = value;
+                        break;
+
+                    case 14:
+                    {
+                        classId[1]      = value;
+                        uint16_t    field   = (classId[0]<<8) | (classId[1]);
+                        upperLayer.ProcessInterfaceClass( field );
+                        break;
+                    }
+
+                    case 15:
+                        logicalName.d0      = value;
+                        break;
+
+                    case 16:
+                        logicalName.d1      = value;
+                        break;
+
+                    case 17:
+                        logicalName.d2      = value;
+                        break;
+
+                    case 18:
+                        logicalName.d3      = value;
+                        break;
+
+                    case 19:
+                        logicalName.d4      = value;
+                        break;
+
+                    case 20:
+                    {
+                        logicalName.d5      = value;
+                        upperLayer.ProcessLogicalName( logicalName );
+                        break;
+                    }
+
+                    case 21:
+                        attributeNumber[0]   = value;
+                        break;
+
+                    case 22:
+                    {
+                        attributeNumber[1]   = value;
+                        uint16_t    field   = (attributeNumber[0]<<8) | (attributeNumber[1]);
+                        upperLayer.ProcessAttributeNumber( field );
+                        break;
+                    }
+
+                    case 23:
+                        fcs[0]   = value;
+                        break;
+
+                    case 24:
+                    {
+                        fcs[1]   = value;
+                        uint16_t    field   = (fcs[0]<<8) | (fcs[1]);
+                        if(field == 0x0dfd)
+                        {
+                            upperLayer.CompleteProcessing();
+                        }
+                        else
+                        {
+                            upperLayer.AbortProcessing();
+                        }
+                        break;
+                    }
+
+                    default:
+                        break;
+                }
+
+                position++;
+            }
+
+        } while( success == true );
+
+        return false;
+    }
+
 
 private:
 
     static const uint8_t    HDLC_FRAME_DELIMIT     = 0x7E;
 
-    ByteStream&  stream;
+    ByteStream&     stream;
+    DLMSParser&     upperLayer;
 
 };
 
@@ -128,20 +380,34 @@ int main()
     printf("DLMS encode/decoder test\n");
 
     {
+        //
+        // frame type: A0
+        // frame length: 19
+        // dest address: 95 
+        // source address upper: 75,54,
+        // source address lower: 68,35
+        // llc bytes: E6E600
+        // GET request normal: C001
+        // invoke id and priority: 81
+        // interface class 8, clock: 0008
+        // logical name, obis code of clock: 0000010000FF
+        // asking for 1st attribute, logical name: 0100
+        // HDLC frame check sequence: 0DFD
+        // HDLC frame close: 7E
+        //
         uint8_t     readClockRequest[] = {0x7E,0xA0,0x19,0x95,0x75,0x54,0x68,0x35,0xE6,0xE6,0x00,0xC0,0x01,0x81,0x00,0x08,0x00,0x00,0x01,0x00,0x00,0xFF,0x01,0x00,0x0D,0xFD,0x7E};
         ByteStream  requestStream( &readClockRequest[0], sizeof(readClockRequest) );
-        HDLCFrame   requestFrame( requestStream );
+        DLMSParser  dlmsParser;
+        HDLCFrame   requestFrame( requestStream, dlmsParser );
 
         bool        success             = false;
-        uint8_t     value               = 0;
 
+        //
+        // Parse the frame.
+        //
         do
         {
-            success = requestFrame.Get_UINT8(value);
-            if(success == true)
-            {
-                printf("%02x ", value);
-            }
+            success = requestFrame.Parse();
 
         } while( success == true );
     }
