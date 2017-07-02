5f6b2515ec24fb797d0664d7724cb43cc44be0cf
Steve Tickle <Steve@BlockWorks.co>
2017-07-02 18:59:55 +0100

    -

--- FileSystemPatch/GenerateFileSystemHash.sh
+++ FileSystemPatch/GenerateFileSystemHash.sh
@@ -6,7 +6,7 @@ rm -f /tmp/Hash
 
 pushd $1
 find . | sort > /tmp/Hash
-cat /tmp/Hash | xargs stat  > /tmp/Hash2
+cat /tmp/Hash | xargs  stat -c "%a %D %F %g %u %n"   > /tmp/Hash2
 cat /tmp/Hash2 >> /tmp/Hash
 md5sum /tmp/Hash
 popd
