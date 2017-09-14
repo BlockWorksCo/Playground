98111e42586babe3f8c27df6d10b03a82f682e7c
Steve Tickle <Steve@BlockWorks.co>
2017-09-14 21:10:00 +0100

    added multiplatformmakefiles

---
+++ MultiPlatformMakefiles/ComponentOne/Build.mk
@@ -0,0 +1,24 @@
+
+#
+# Source directories
+#
+VPATH		+=	$(PROJECT_BASE)/ComponentOne
+
+
+#
+# Include directories.
+#
+INCLUDES	+=	-I$(PROJECT_BASE)/ComponentOne
+
+
+#
+# Objects to build.
+#
+OBJS		+=	ComponentOne.o
+    		  		
+
+#
+# Libraries.
+#
+LIBS		+=
+
---
+++ MultiPlatformMakefiles/ComponentTwo/Build.mk
@@ -0,0 +1,24 @@
+
+#
+# Source directories
+#
+VPATH		+=	$(PROJECT_BASE)/ComponentTwo
+
+
+#
+# Include directories.
+#
+INCLUDES	+=	-I$(PROJECT_BASE)/ComponentTwo
+
+
+#
+# Objects to build.
+#
+OBJS		+=	ComponentTwo.o
+    		  		
+
+#
+# Libraries.
+#
+LIBS		+=
+
