2a9fee2cdd037bf309787b5cd37f958fea6a0df5
Steve Tickle <Steve.Tickle@CyanConnode.com>
2017-10-04 16:25:30 +0100

    made a better name

--- GuruxXML2PDU/MakeMe.sh
+++ GuruxXML2PDU/MakeMe.sh
@@ -6,10 +6,10 @@ mvn clean
 mvn compile
 popd
 
-pushd gurux.dlms.java/gurux.dlms.client.example.java
+pushd gurux.dlms.java/pdu2xml
 mvn clean
 mvn package
 popd
 
-mv gurux.dlms.java/gurux.dlms.client.example.java/target/gurux.dlms.client.example.java-0.0.1-SNAPSHOT.jar pdu2xml.jar
+mv gurux.dlms.java/pdu2xml/target/gurux.dlms.client.example.java-0.0.1-SNAPSHOT.jar pdu2xml.jar
 java -jar pdu2xml.jar c4018100090c303030303030303130303031
