e211e9e04f153388a34058840a34a2281d588d4a
Steve Tickle <Steve.Tickle@CyanConnode.com>
2017-10-04 16:31:27 +0100

    added xml2pdu

--- GuruxXML2PDU/MakeMe.sh
+++ GuruxXML2PDU/MakeMe.sh
@@ -10,6 +10,14 @@ pushd gurux.dlms.java/pdu2xml
 mvn clean
 mvn package
 popd
-
 mv gurux.dlms.java/pdu2xml/target/gurux.dlms.client.example.java-0.0.1-SNAPSHOT.jar pdu2xml.jar
+
+pushd gurux.dlms.java/xml2pdu
+mvn clean
+mvn package
+popd
+mv gurux.dlms.java/xml2pdu/target/gurux.dlms.client.example.java-0.0.1-SNAPSHOT.jar xml2pdu.jar
+
+
 java -jar pdu2xml.jar c4018100090c303030303030303130303031
+
