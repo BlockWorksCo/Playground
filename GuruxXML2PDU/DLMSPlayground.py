9885ae958e919d62cd18bc377801595ebc442871
Steve Tickle <Steve.Tickle@CyanConnode.com>
2017-10-06 14:26:59 +0100

    -

--- GuruxXML2PDU/DLMSPlayground.py
+++ GuruxXML2PDU/DLMSPlayground.py
@@ -3,6 +3,11 @@
 
 import DLMS
 import xmltodict
+import time
+import serial
+import sys
+import binascii
+
 
 
 
@@ -64,4 +69,114 @@ def CreateSetRequest_OctetString(ic, obis, attributeId, value):
     return hdlc
 
 
+
+
+
+AARETemplate  = \
+"""
+<AssociationRequest>
+  <ApplicationContextName Value="%s" />
+  <SenderACSERequirements Value="1" />
+  <MechanismName Value="%s" />
+  <CallingAuthentication Value="%s" />
+  <InitiateRequest>
+    <ProposedDlmsVersionNumber Value="06" />
+    <ProposedConformance>
+      <ConformanceBit Name="Action" />
+      <ConformanceBit Name="EventNotification" />
+      <ConformanceBit Name="SelectiveAccess" />
+      <ConformanceBit Name="Set" />
+      <ConformanceBit Name="Get" />
+      <ConformanceBit Name="InformationReport" />
+      <ConformanceBit Name="MultipleReferences" />
+      <ConformanceBit Name="BlockTransferWithAction" />
+      <ConformanceBit Name="BlockTransferWithSetOrWrite" />
+      <ConformanceBit Name="BlockTransferWithGetOrRead" />
+    </ProposedConformance>
+    <ProposedMaxPduSize Value="FFFF" />
+  </InitiateRequest>
+</AssociationRequest>
+"""
+
+def CreateAARE(contextName, mechanismName, password):
+    """
+    DLMSPlayground.CreateAARE('LN', 'Low', '3132333435363738')
+    """
+    xml     = AARETemplate%(contextName, mechanismName, password) 
+    d       = xmltodict.parse(xml)
+    hdlc    = DLMS.DictToHDLC(d)
+
+    return hdlc
+
+
+
+def CreateSNRM():
+    """
+    DLMSPlayground.CreateSNRM()
+    """
+    #return '7ea023000200234193f736818014050200b4060200b407040000000108040000000115ca7e'
+    return '7ea00a410002002373fd607e'
+
+
+
+
+def CreateDISC():
+    """
+    DLMSPlayground.CreateDISC()
+    """
+    return '7ea00a00020023415341d27e'
+
+
+
+
+
+
+
+def OpenPortToMeter(portName):
+    """
+    port    = OpenPortToMeter('/dev/ttyUSB2')
+    """
+    port = serial.Serial(
+        port=portName,
+        baudrate=9600,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS
+    )
+
+    port.isOpen()
+
+    return port
+
+
+def SendHDLCToMeter(port, hdlcHex):
+    """
+    """
+    port.write( binascii.unhexlify(hdlcHex) )
+
+
+def GetResponseFromMeter(port):
+    """
+    """
+    response    = ''
+    while port.inWaiting() > 0:
+        response += port.read(1)
+
+    response    = binascii.hexlify(response)
+    #print(response)
+    return response
+
+
+
+
+
+def TestMe():
+    """
+    """
+    p=OpenPortToMeter('/dev/ttyUSB2')
+    snrm=CreateSNRM()
+    SendHDLCToMeter(p, snrm)
+    time.sleep(1.0)
+    GetResponseFromMeter(p)
+ 
 
