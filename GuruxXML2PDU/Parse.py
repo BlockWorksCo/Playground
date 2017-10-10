94b0af27e7b888de7ea5f9f4810c9cddd6d958f6
Steve Tickle <Steve@BlockWorks.co>
2017-10-10 21:48:03 +0100

    -

--- GuruxXML2PDU/Parse.py
+++ GuruxXML2PDU/Parse.py
@@ -4,11 +4,13 @@
 
 
 
+import inspect
 import binascii
 import sys
 
 ARRAY       = 0x01
 STRUCTURE   = 0x02
+BOOLEAN     = 0x03
 STRING_UTF8 = 0x0b
 OCTET_STRING= 0x09
 UINT8       = 0x11
@@ -18,11 +20,17 @@ ENUM        = 0x16
 
 
 
+def Indent():
+    """
+    """
+    return ' '*len(inspect.stack())
+
+
 def ParseArray(pdu):
     """
     """
     length = ord(pdu[0])
-    print('array of length %d'%length) 
+    print('%sarray of length %d'%(Indent(),length)) 
 
     position    = 1
     for i in range(length):
@@ -37,7 +45,7 @@ def ParseOctetString(pdu):
     length = ord(pdu[0])
     value   = pdu[1:1+length]
     value   = binascii.hexlify(value)
-    print('octet string of length %d = [%s]'%(length,value))
+    print('%soctet string of length %d = [%s]'%(Indent(),length,value))
 
     return 1+length
 
@@ -46,7 +54,7 @@ def ParseStructure(pdu):
     """
     """
     numberOfFields = ord(pdu[0])
-    print('structure with %d fields'%numberOfFields) 
+    print('%sstructure with %d fields'%(Indent(),numberOfFields)) 
 
     position    = 1
     for i in range(numberOfFields):
@@ -59,9 +67,38 @@ def ParseUint8(pdu):
     """
     """
     value  = ord(pdu[0])
-    print('UINT8 of value %02x'%value)
+    print('%sUINT8 of value %02x'%(Indent(),value))
 
     return 1
+
+
+def ParseInt8(pdu):
+    """
+    """
+    value  = ord(pdu[0])
+    print('%sINT8 of value %02x'%(Indent(),value))
+
+    return 1
+
+
+def ParseBoolean(pdu):
+    """
+    """
+    value  = ord(pdu[0])
+    print('%sBOOLEAN of value %02x'%(Indent(),value))
+
+    return 1
+
+
+def ParseEnum(pdu):
+    """
+    """
+    hi  = ord(pdu[0])
+    lo  = ord(pdu[1])
+    value   = (hi<<8)|lo
+    print('%sENUM of value %02x'%(Indent(),value))
+
+    return 3
 
 
 def ParseUint16(pdu):
@@ -70,7 +107,7 @@ def ParseUint16(pdu):
     hi  = ord(pdu[0])
     lo  = ord(pdu[1])
     value   = (hi<<8)|lo
-    print('UINT16 of value %04x'%value)
+    print('%sUINT16 of value %04x'%(Indent(),value))
 
     return 2
 
@@ -94,6 +131,12 @@ def ParseAXDR(pdu):
             position    += ParseUint16(pdu[position:])
         elif tag == OCTET_STRING:
             position    += ParseOctetString(pdu[position:])
+        elif tag == INT8:
+            position    += ParseInt8(pdu[position:])
+        elif tag == ENUM:
+            position    += ParseEnum(pdu[position:])
+        elif tag == BOOLEAN:
+            position    += ParseBoolean(pdu[position:])
         else:
             print('unknown tag %02x'%tag)
             sys.exit(-1)
