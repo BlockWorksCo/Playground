f08a51bf0f58f81826445606ce904efdc193a4fb
Steve Tickle <Steve@BlockWorks.co>
2017-10-16 23:40:53 +0100

    -

--- GuruxXML2PDU/EncodeAXDR.py
+++ GuruxXML2PDU/EncodeAXDR.py
@@ -18,21 +18,25 @@ def EncodeAXDR(d):
     """
     hexData = ''
     typeName    = type(d).__name__
-    #print(typeName)
-    
-    if typeName == 'list' or typeName == 'dict' or typeName == 'OrderedDict':
-        
-        print('%svector type: [%s]'%(Indent(),str(d)))
+
+    if typeName == 'unicode':
+        print('%sScalar type [%s] is %s'%(Indent(),str(d),typeName))
+    else:
+        print(d.keys())
 
         for key in d.keys():
             element = d[key]
+            print('%sComposite %s %s'%(Indent(),key, typeName))
 
             if key == 'Array':
-                print('%sArray of %d elements'%(Indent(),len(d[key])))
+                print('%sArray of %d elements'%(Indent(),len(element)))
                 i   = 0
-                for arrayElement in d[key]:
-                    print('%sItem %d'%(Indent(),i))
-                    hexData = EncodeAXDR(arrayElement)
+                for arrayElement in element:
+                    print('%sItem %d [%s]'%(Indent(),i,str(arrayElement)))
+                    if arrayElement[0] == '@':
+                        print('%sIgnoring [%s]'%(Indent(),str(d)))
+                    else:
+                        hexData = EncodeAXDR(d[arrayElement])
                     i   = i+1 
 
             elif key == 'Data':
@@ -40,7 +44,7 @@ def EncodeAXDR(d):
                 hexData = EncodeAXDR(d[key])
 
             elif key == 'ArrayElement':
-                print('%sArrayElement'%(Indent()))
+                print('%s->ArrayElement'%(Indent()))
                 hexData = EncodeAXDR(d[key])
 
             elif key == 'Structure':
@@ -51,18 +55,6 @@ def EncodeAXDR(d):
                     hexData = EncodeAXDR(element)
                     i   = i+1 
 
-            else:
-                print('Unknown element type [%s]'%key)
-
-    else:
-        if d[0] == '@':
-            print('%signoring attribute %s'%(Indent(),d))
-        else:
-            if d == 'ArrayElement':
-                print('%sArrayElement'%(Indent()))
-                hexData = EncodeAXDR(d[key])
-
-            print('%sScalar type [%s] is %s'%(Indent(),str(d),typeName))
 
     return hexData 
 
