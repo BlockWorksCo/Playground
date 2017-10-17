93d9506ee33ccc2ec7dd77d62bd84da47ec95a39
Steve Tickle <Steve.Tickle@CyanConnode.com>
2017-10-17 14:03:18 +0100

    -

--- GuruxXML2PDU/EncodeAXDR.py
+++ GuruxXML2PDU/EncodeAXDR.py
@@ -24,20 +24,32 @@ def EncodeAXDR(name,d):
 
     else:
         typeName    = type(d).__name__
-        print('[in %s of type %s]'%(name, typeName))
+        #print('[in %s of type %s]'%(name, typeName))
 
-        if typeName == 'dict' or typeName == 'OrderedDict':
-            children    = d.keys()
-            print('%d children %s (dict)'%(len(children),children))
-            for child in children:
-                EncodeAXDR(child, d[child])
+        if name == 'OctetString':
+            hexData = ('OctetString [%s]'%(d['#text']))
 
-        if typeName == 'list':
-            print('%d children (list)'%(len(d)))
-            i   = 0
-            for child in d:
-                EncodeAXDR(name+'_%d'%i, child)
-                i   = i + 1
+        elif name == 'Int8':
+            hexData = ('Int8 [%s]'%(d['#text']))
+
+        elif name == 'Uint16':
+            hexData = ('Uint16 [%s]'%(d['#text']))
+
+        else:
+            if typeName == 'dict' or typeName == 'OrderedDict':
+                children    = d.keys()
+                if name == 'ArrayElement' or name == 'Field':
+                    hexData = '%s of %d :'%(name,len(children))
+                for child in children:
+                    hexData = hexData + EncodeAXDR(child, d[child])
+
+            if typeName == 'list':
+                i   = 0
+                if name == 'ArrayElement' or name == 'Field':
+                    hexData = '%s of %d :'%(name,len(d))
+                for child in d:
+                    hexData = hexData + EncodeAXDR(' ', child)
+                    i   = i + 1
 
     return hexData 
 
@@ -48,6 +60,6 @@ if __name__ == '__main__':
     """
     xml     = open(sys.argv[1]).read()
     d       = xmltodict.parse(xml)
-    hexPDU  = EncodeAXDR('xml',d)
+    hexPDU  = EncodeAXDR(' ',d)
     print(hexPDU)
 
