8fafdd4d946e0a8596bc5a46b7f4c67d70875ce9
Steve Tickle <Steve.Tickle@CyanConnode.com>
2017-10-17 14:12:38 +0100

    -

--- GuruxXML2PDU/EncodeAXDR.py
+++ GuruxXML2PDU/EncodeAXDR.py
@@ -24,32 +24,29 @@ def EncodeAXDR(name,d):
 
     else:
         typeName    = type(d).__name__
-        #print('[in %s of type %s]'%(name, typeName))
 
         if name == 'OctetString':
-            hexData = ('OctetString [%s]'%(d['#text']))
+            hexData = '09%s'%(d['#text'])
 
         elif name == 'Int8':
-            hexData = ('Int8 [%s]'%(d['#text']))
+            hexData = '0f%s'%(d['#text'])
 
         elif name == 'Uint16':
-            hexData = ('Uint16 [%s]'%(d['#text']))
+            hexData = '12%s'%(d['#text'])
 
         else:
             if typeName == 'dict' or typeName == 'OrderedDict':
                 children    = d.keys()
-                if name == 'ArrayElement' or name == 'Field':
-                    hexData = '%s of %d :'%(name,len(children))
                 for child in children:
                     hexData = hexData + EncodeAXDR(child, d[child])
 
             if typeName == 'list':
-                i   = 0
-                if name == 'ArrayElement' or name == 'Field':
-                    hexData = '%s of %d :'%(name,len(d))
+                if name == 'ArrayElement':
+                    hexData = '01%02x'%(len(d))
+                if name == 'Field' or name == 'Field':
+                    hexData = '02%02x'%(len(d))
                 for child in d:
                     hexData = hexData + EncodeAXDR(' ', child)
-                    i   = i + 1
 
     return hexData 
 
