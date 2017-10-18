332edbe4cc8450798250aa926a489a77e60fd5bc
Steve Tickle <Steve@BlockWorks.co>
2017-10-18 19:02:07 +0100

    -

--- GuruxXML2PDU/VirtualMeter/hexlify.py
+++ GuruxXML2PDU/VirtualMeter/hexlify.py
@@ -8,5 +8,5 @@ import sys
 
 if __name__ == '__main__':
     for line in sys.stdin:
-        sys.stdout.write(binascii.unhexlify(line.replace('\n','').replace('\r','')))
+        sys.stdout.write(binascii.hexlify(line))
 
