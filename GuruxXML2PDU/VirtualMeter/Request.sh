4c8375d2c895be08d2e7e82f7ec68fbf2f58c589
Steve Tickle <Steve@BlockWorks.co>
2017-10-19 23:52:37 +0100

    -

--- GuruxXML2PDU/VirtualMeter/Request.sh
+++ GuruxXML2PDU/VirtualMeter/Request.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 
-cat ../Examples/hdlc5.hdlc | ./unhexlify.py | netcat localhost 9999 | ./hexlify.py 
+cat ../Examples/hdlc5.hdlc | ./unhexlify.py | netcat localhost 9999 | ./hexlify.py > temp.pdu
+PYTHONPATH=.. PATH=$PATH:.. ../pdu2xml.sh temp.pdu
 
