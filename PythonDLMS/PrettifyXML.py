0b342a118200929115d67e614fd7367722291e7d
Steve Tickle <Steve@BlockWorks.co>
2017-10-24 20:19:51 +0100

    added PrettifyXML.py

---
+++ PythonDLMS/PrettifyXML.py
@@ -0,0 +1,36 @@
+
+
+
+from xml.etree import ElementTree
+import sys
+
+def indent(elem, level=0, more_sibs=False):
+    i = "\n"
+    if level:
+        i += (level-1) * '  '
+    num_kids = len(elem)
+    if num_kids:
+        if not elem.text or not elem.text.strip():
+            elem.text = i + "  "
+            if level:
+                elem.text += '  '
+        count = 0
+        for kid in elem:
+            indent(kid, level+1, count < num_kids - 1)
+            count += 1
+        if not elem.tail or not elem.tail.strip():
+            elem.tail = i
+            if more_sibs:
+                elem.tail += '  '
+    else:
+        if level and (not elem.tail or not elem.tail.strip()):
+            elem.tail = i
+            if more_sibs:
+                elem.tail += '  '
+
+if __name__ == '__main__':
+
+    root = ElementTree.parse(sys.argv[1]).getroot()
+    indent(root)
+    ElementTree.dump(root)
+
