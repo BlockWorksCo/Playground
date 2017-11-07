cd195fdbadbc3fc9277f378e7adf040fb0a594d0
Steve Tickle <Steve@BlockWorks.co>
2017-11-07 23:41:36 +0000

    -

--- FusEd/FrontEnd.py
+++ FusEd/FrontEnd.py
@@ -3,11 +3,54 @@
 
 
 import curses
+import curses.textpad
 import os
 import time
 import FusEd
 import threading
 import sys
+
+
+class FrontEnd:
+    """
+    """
+
+
+    def __init__(self):
+        """
+        """
+        stdscr = curses.initscr()
+
+        #curses.noecho()
+        #curses.echo()
+
+        begin_x = 20
+        begin_y = 7
+        height = 5
+        width = 40
+        win = curses.newwin(height, width, begin_y, begin_x)
+        tb = curses.textpad.Textbox(win)
+        text = tb.edit()
+        curses.addstr(4,1,text.encode('utf_8'))
+
+    
+    def MainLoop(self):
+        """
+        """
+
+        hw = "Hello world!"
+        while 1:
+            c = stdscr.getch()
+            if c == ord('p'): 
+                pass
+            elif c == ord('q'): 
+                break # Exit the while()
+            elif c == curses.KEY_HOME: 
+                x = y = 0
+
+        curses.endwin()
+
+
 
 
 
@@ -26,10 +69,11 @@ if __name__ == '__main__':
     t.daemon    = True;
     t.start()
 
-    while True:
-        time.sleep(1)
-        print('Tick...')
-        #print(open('./tmp/'+fileName).read())
+    FrontEnd().Run()
+    #while True:
+    #    time.sleep(1)
+    #    print('Tick...')
+    #    #print(open('./tmp/'+fileName).read())
 
 
 
