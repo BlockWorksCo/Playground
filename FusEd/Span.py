509bba022403b45ad5af430292f64c73922163fa
Steve Tickle <Steve@BlockWorks.co>
2017-11-10 23:22:17 +0000

    -

---
+++ FusEd/Span.py
@@ -0,0 +1,43 @@
+
+
+
+
+
+def SpanContainingPoint(spans, point):
+
+    for start,end in spans:
+        if start <= point and end >= point:
+            return (start,end)
+
+    return None
+
+
+def AddSpan(spans, start,end):
+
+    sS,sE   = SpanContainingPoint(spans, start)
+    eS,eE   = SpanContainingPoint(spans, end)
+
+    startContainerIndex = spans.index( (sS,sE) )
+    endContainerIndex   = spans.index( (eS,eE) )
+
+    if startContainerIndex == endContainerIndex:
+        spans.remove( (sS,sE) )
+        spans.insert(startContainerIndex, (end,eE))
+        spans.insert(startContainerIndex, (start,end))
+        spans.insert(startContainerIndex, (sS,start))
+
+
+
+    return spans
+
+
+
+if __name__ == '__main__':
+
+    spans   = [(0,100)]
+    AddSpan(spans, 10,20)
+    AddSpan(spans, 0,5)
+
+    print(spans)
+    
+
