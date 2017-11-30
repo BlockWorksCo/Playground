59f3a3b921c169f1da59d1168abe18b6ab9fce57
Steve Tickle <Steve@BlockWorks.co>
2017-11-30 21:08:09 +0000

    -

--- FusEd/t.py
+++ FusEd/t.py
@@ -2,17 +2,35 @@
 
 
 
+import time
+import multiprocessing
 from multiprocessing.managers import BaseManager, NamespaceProxy
 
 
 
 
-class TestClass(object):
-    def __init__(self, a):
-        self.a = a
 
-    def b(self):
-        print(self.a)
+
+
+class TestClass(object):
+
+    handles = []
+
+    def SetHandles(self,handles):
+        self.handles  = handles
+
+    def GetHandles(self):
+        return self.handles
+
+
+def Process(fs):
+
+    while True:
+        time.sleep(1.0)
+        print('tick...')
+        print(fs.handles)
+
+
 
 
 
@@ -21,26 +39,37 @@ class MyManager(BaseManager): pass
 class TestProxy(NamespaceProxy):
     # We need to expose the same __dunder__ methods as NamespaceProxy,
     # in addition to the b method.
-    _exposed_ = ('__getattribute__', '__setattr__', '__delattr__', 'b')
+    _exposed_ = ('__getattribute__', '__setattr__', '__delattr__', 'SetHandles','GetHandles')
 
-    def b(self):
+    def SetHandles(self, handles):
         callmethod = object.__getattribute__(self, '_callmethod')
-        return callmethod('b')
+        return callmethod('SetHandles', (handles,))
+
+    def GetHandles(self):
+        callmethod = object.__getattribute__(self, '_callmethod')
+        return callmethod('GetHandles')
 
 MyManager.register('test', TestClass, TestProxy)
 
 
 
-
-
 if __name__ == '__main__':
+
     manager = MyManager()
     manager.start()
-    t = TestClass(1)
-    print( t.a)
-    mt = manager.test(2)
-    print(mt.a)
-    mt.a = 5
-    mt.b()
-    print( t.a)
+
+    mt = manager.test()
+    mt.a = []
+
+    t = multiprocessing.Process(target=Process, args=(mt,) )
+    t.daemon    = True;
+    t.start()
+
+    mt.SetHandles([1,2,3])
+    while True:
+        time.sleep(1.0)
+        print('tock...')
+        ll  = mt.GetHandles()
+        ll.append(',')
+        mt.SetHandles( ll )
 
