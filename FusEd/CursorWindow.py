b4f5a5001c5098d390bd86d2dcaf5b6e3237dc41
Steve Tickle <Steve@BlockWorks.co>
2018-01-12 21:19:44 +0000

    -

--- FusEd/CursorWindow.py
+++ FusEd/CursorWindow.py
@@ -10,15 +10,42 @@ import logging
 
 class CursorWindow:
 
-    def __init__(self, fh, rangeStart,rangeEnd):
+    def __init__(self, left,top,width,height, x=0,y=0):
         self.logger     = logging.getLogger('CursorWindow')
+
+        self.left   = left
+        self.top    = top
+        self.width  = width
+        self.height = height
+
+        self.cx     = 0
+        self.cy     = 0
+
+
+
+    def MoveTo(self, x,y):
+
+        dx  = x - self.cx
+        dy  = y - self.cy
+
+
+
+
+
 
 
 
 class Tests(unittest.TestCase):
 
     def test_one(self):
-        self.assertEqual(True, True )
+
+        window  = CursorWindow(0,0,100,100, 0,0)
+        window.MoveTo(10,10)
+
+        self.assertEqual( window.left,  0 )
+        self.assertEqual( window.top,   0 )
+        self.assertEqual( window.cx,    0 )
+        self.assertEqual( window.cy,    0 )
 
 
 
