750fd9a897c4724473714b2e37d1d64f194cfbdf
Steve Tickle <Steve@BlockWorks.co>
2018-02-15 21:28:40 +0000

    -

--- BTUIScan.py
+++ BTUIScan.py
@@ -84,6 +84,8 @@ class Scanner:
 
         #adapter.SetDiscoveryFilter({'UUIDs':["6e400001-b5a3-f393-e0a9-e50e24dcca9e"]})
         adapter.SetDiscoveryFilter({'UUIDs':["0000feaa-0000-1000-8000-00805f9b34fb"]})
+                                            #4db9-0ef2-f9d6-fdc9-717a-000000000000
+
         #adapter.SetDiscoveryFilter({})
         adapter.StartDiscovery()
 
