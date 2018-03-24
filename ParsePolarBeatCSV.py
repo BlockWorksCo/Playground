7c37a1046fb9666048bd35a08b0e4bcf03bf8782
Steve Tickle <Steve@BlockWorks.co>
2018-03-24 21:07:06 +0000

    .

---
+++ ParsePolarBeatCSV.py
@@ -0,0 +1,28 @@
+
+
+
+import csv
+import sys
+import re
+
+
+with open(sys.argv[1], 'rb') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    rowCount    = 0
+    hrData      = []
+    for row in spamreader:
+        if rowCount > 2:
+            #print(row[1])
+            #print(row[2])
+            timeTriple  = re.compile('([0-9]+):([0-9]+):([0-9]+)').findall(row[1])[0]
+            #print(timeTriple)
+            timestamp   = int(timeTriple[0])*3600 + int(timeTriple[1])*60 + int(timeTriple[2])
+            hr          = int(row[2])
+            hrData.append([timestamp,hr])
+            #print(hr)
+            #print(timestamp)
+        rowCount    = rowCount+1
+
+    print(hrData)
+
+
