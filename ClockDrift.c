cc43b7c7745addc8546cf5c92f88a49d11ab4a1a
Steve Tickle <Steve@ZooKoo.com>
2018-06-22 00:10:44 +0100

    .

--- ClockDrift.c
+++ ClockDrift.c
@@ -14,7 +14,7 @@ typedef struct
 {
     uint32_t    serverTime; // truth.
     uint32_t    deviceTime; // drifting time on device.
-    uint32_t    reference;
+    uint32_t    token;
     uint32_t    value;
 
 } Sample;
@@ -29,17 +29,40 @@ uint32_t DriftTime( uint32_t serverTime, uint32_t timeSinceSync )
 
 void GenerateClockDriftData( Sample* samples, uint32_t numberOfSamples )
 {
-    uint32_t    lastReferenceReceived   = rand();
+    uint32_t    lastTokenReceived   = rand();
 
     for(uint32_t i=0; i<numberOfSamples; i++)
     {
-        samples[i].reference     = lastReferenceReceived;
+        samples[i].token         = lastTokenReceived;
         samples[i].serverTime    = i;
         samples[i].deviceTime    = DriftTime( samples[i].serverTime, i );
 
         if( samples[i].serverTime % 3600 == 0 )
         {
-            lastReferenceReceived   = rand();
+            lastTokenReceived   = rand();
+        }
+    }
+}
+
+
+
+
+void RecoverSamples( Sample* samples, uint32_t numberOfSamples )
+{
+    uint32_t    lastTokenIndex  = 0;
+    uint32_t    lastToken       = samples[lastTokenIndex].token;
+
+    for(uint32_t i=1; i<numberOfSamples; i++)
+    {
+        if( samples[i].token != lastToken )
+        {
+            float   deviceDelta = samples[i].deviceTime - samples[lastTokenIndex].deviceTime; 
+            float   serverDelta = samples[i].serverTime - samples[lastTokenIndex].serverTime; 
+            float   driftRate   = deviceDelta / serverDelta;
+            printf("- %08d %08d %08x %f\n", samples[i].serverTime, samples[i].deviceTime, samples[i].token, driftRate );
+
+            lastTokenIndex   = i;
+            lastToken   = samples[lastTokenIndex].token;
         }
     }
 }
@@ -53,8 +76,10 @@ int main()
     GenerateClockDriftData( &samples[0], numberOfSamples );
     for(uint32_t i=0; i<numberOfSamples; i++)
     {
-        printf("%08d %08d %08x\n", samples[i].serverTime, samples[i].deviceTime, samples[i].reference );
+        printf("%08d %08d %08x\n", samples[i].serverTime, samples[i].deviceTime, samples[i].token );
     }
+
+    RecoverSamples( &samples[0], numberOfSamples );
 
     return 0;
 }
