aada674dbedb533fea620d3919658b6ab5604f8e
Steve Tickle <Steve@ZooKoo.com>
2018-06-29 23:42:43 +0100

    .

--- omnibox-ui/src/components/Radio.vue
+++ omnibox-ui/src/components/Radio.vue
@@ -1,16 +1,54 @@
+
 <template>
   <v-container fluid>
     <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <h1>Radio</h1>
+      <v-layout>
+
+        <v-flex xs6>
+           <v-card class="ma-5">
+            <v-card-media src="https://raw.githubusercontent.com/ijklim/simon-game/gh-pages/assets/img/bg--game-pad.jpg" height="200px" ></v-card-media>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+                <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn color=success>Share</v-btn>
+              <v-btn flat color="orange">Explore</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+
+        <v-flex xs6 sm6>
+           <v-card class="ma-5">
+            <v-card-media src="https://raw.githubusercontent.com/ijklim/simon-game/gh-pages/assets/img/bg--game-pad.jpg" height="200px" ></v-card-media>
+            <v-card-title primary-title>
+              <div>
+                <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+                <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+              </div>
+            </v-card-title>
+            <v-card-actions>
+              <v-btn color=success>Share</v-btn>
+              <v-btn flat color="orange">Explore</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-flex>
+
       </v-layout>
     </v-slide-y-transition>
   </v-container>
 </template>
 
 <script>
+
+import VCard from 'vuetify/es5/components/VCard'
+import VCardMedia from 'vuetify/es5/components/VCard/VCardMedia';
+import VCardTitle from 'vuetify/es5/components/VCard/VCardTitle';
+
 export default {
+  components: { VCard, VCardMedia, VCardTitle },
   name: 'Radio',
   props: {
     msg: String
