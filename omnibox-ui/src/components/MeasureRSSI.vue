224a2ec50569c3b9d33fcf274755779758ed8cc9
Steve Tickle <Steve@ZooKoo.com>
2018-07-01 20:21:26 +0100

    .

--- omnibox-ui/src/components/MeasureRSSI.vue
+++ omnibox-ui/src/components/MeasureRSSI.vue
@@ -122,18 +122,58 @@
 
 
 import VueCharts from 'vue-chartjs'
-import { Bar, Line } from 'vue-chartjs'
 
 <script>
+  import {Line} from 'vue-chartjs' // We specify what type of chart we want from vue-chartjs and the mixins module
 export default {
+  extends: Line,
   name: 'MeasureRSSI',
   props: {
     msg: String
   },
   data () {
       return {
-        e1: 0
+        e1: 0,
+        datacollection: {
+            //Data to be represented on x-axis
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+            datasets: [{
+               label: 'Data One',
+               backgroundColor: '#f87979',
+               pointBackgroundColor: 'white',
+               borderWidth: 1,
+               pointBorderColor: '#249EBF',
+               //Data to be represented on y-axis
+               data: [40, 20, 30, 50, 90, 10, 20, 40, 50, 70, 90, 100]
+            }]
+         },
+        options: {
+            scales: {
+               yAxes: [{
+                  ticks: {
+                     beginAtZero: true
+                  },
+                  gridLines: {
+                     display: true
+                  }
+               }],
+               xAxes: [{
+                  gridLines: {
+                     display: false
+                  }
+               }]
+            },
+            legend: {
+               display: true
+            },
+            responsive: true,
+            maintainAspectRatio: false
+         }
       }
+    },
+  mounted () {
+      // this.chartData is created in the mixin
+      this.renderChart(this.chartData, this.options)
     }
 }
 </script>
