ac93b425b8d97777be78b2b081d436818baf3130
Steve Tickle <Steve@ZooKoo.com>
2018-07-02 22:59:15 +0100

    .

--- omnibox-ui/src/components/LineChart.vue
+++ omnibox-ui/src/components/LineChart.vue
@@ -2,23 +2,26 @@
 
 <script>
 
-// CommitChart.js
 import { Line } from 'vue-chartjs'
 
 export default {
   extends: Line,
-  mounted () {
-    // Overwriting base render method with actual data.
-    this.renderChart({
+  props: {
+    options: {maintainAspectRatio:false, responsive:false},
+    data: {
       labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
       datasets: [
         {
           label: 'GitHub Commits',
           backgroundColor: '#f87979',
-          data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+          data: [90, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
         }
       ]
-    })
+    }
+  },
+  mounted () {
+    // Overwriting base render method with actual data.
+    this.renderChart( this.data, this.options)
   }
 }
 
--- omnibox-ui/src/components/MeasureRSSI.vue
+++ omnibox-ui/src/components/MeasureRSSI.vue
@@ -49,8 +49,8 @@
                                <h3 class="headline mb-0">Step 2: Monitor RSSI values</h3>
                              </v-card-title>
                            </v-flex>
-                           <v-flex xs12 align-end flexbox>
-                                <div><line-chart :chart-data="datacollection" :options="options" /></div>
+                           <v-flex xs12 align-end flexbox height=500px>
+                                <div><line-chart :chart-data="datacollection" :options="options" :height="500" :width="1000"/></div>
                            </v-flex>
                         </v-layout>
                     </v-container>
@@ -170,35 +170,21 @@ export default {
                }],
                xAxes: [{
                   gridLines: {
-                     display: false
+                     display: true
                   }
                }]
             },
             legend: {
-               display: true
+               display: false
             },
-            responsive: true,
+            responsive: false,
             maintainAspectRatio: false
-         }
+         },
+
+        myStyles: "max-width:100px max-height:300px width:333px height:444px",
       }
+
     },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
