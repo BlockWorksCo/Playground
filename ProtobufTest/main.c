f6c5cb0798bbe65b1aeec41010ed8c905e447ed8
Steve Tickle <Steve@ZooKoo.com>
2018-07-06 23:15:40 +0100

    .

--- ProtobufTest/main.c
+++ ProtobufTest/main.c
@@ -8,6 +8,16 @@
 #include <pb_decode.h>
 #include "message.pb.h"
 
+
+
+
+
+
+bool encodeList(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
+{
+    return true;
+}
+
 int main(int argc, char **argv)
 {
     {
@@ -16,9 +26,13 @@ int main(int argc, char **argv)
         message.a   = 33;
         message.b   = 44;
         message.has_b    = true;
+        message.list.funcs.encode   = &encodeList;
 
-        uint8_t buffer[AMessage_size];
-        pb_ostream_t stream = pb_ostream_from_buffer(buffer, sizeof(buffer));
+        size_t  bufferSize  = 0;
+        pb_get_encoded_size( &bufferSize, AMessage_fields, &message );
+        printf("encoded size = %zu\n", bufferSize);
+        uint8_t*    buffer  = malloc(bufferSize);
+        pb_ostream_t stream = pb_ostream_from_buffer(buffer, bufferSize );
         
         /* Now encode it and check if we succeeded. */
         if (pb_encode(&stream, AMessage_fields, &message))
