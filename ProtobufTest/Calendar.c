cab84e22f28ecbe014a8329f203ffa6ea894ab53
Steve Tickle <Steve@ZooKoo.com>
2018-07-19 21:11:31 +0100

    .

--- ProtobufTest/Calendar.c
+++ ProtobufTest/Calendar.c
@@ -25,6 +25,60 @@ void printHexData(uint8_t* data, uint32_t numberOfBytes)
         printHex(data[i]);                                                                      
     }                                                                                           
 }                                                                                               
+
+
+
+
+
+
+
+bool decodeDaySchedule(pb_istream_t *stream, const pb_field_t *field, void **arg)                          
+{                                                                                                  
+    TariffDaySchedule msg = TariffDaySchedule_init_zero;                                                       
+                                                                                                   
+    pb_decode(stream, TariffDaySchedule_fields, &msg);                                                   
+
+    printf("hours=%d minutes=%d seconds=%d tariffId=%d\n",msg.hours, msg.minutes, msg.seconds, msg.tariffId);
+                                                                                                   
+    return true;                                                                                   
+}                                                                                                  
+
+
+
+bool decodeDayProfile(pb_istream_t *stream, const pb_field_t *field, void **arg)                          
+{                                                                                                  
+    DayProfile msg = DayProfile_init_zero;                                                       
+
+    msg.daySchedule.funcs.decode  = &decodeDaySchedule;                                                  
+    msg.daySchedule.arg  = NULL;                                                                      
+
+    pb_decode(stream, DayProfile_fields, &msg);                                                   
+
+    printf("dayId=%d\n\n",msg.identifier);
+                                                                                                   
+    return true;                                                                                   
+}                                                                                                  
+
+
+bool decodeRateCalendar(pb_istream_t *stream, const pb_field_t *field, void **arg)                          
+{                                                                                                  
+    RateCalendar msg = RateCalendar_init_zero;                                                       
+
+    msg.seasonProfiles.funcs.decode  = NULL;
+    msg.seasonProfiles.arg  = NULL;                                                                      
+
+    msg.weekProfiles.funcs.decode  = NULL;
+    msg.weekProfiles.arg  = NULL;                                                                      
+
+    msg.dayProfiles.funcs.decode  = &decodeDayProfile;
+    msg.dayProfiles.arg  = NULL;                                                                      
+
+    pb_decode(stream, RateCalendar_fields, &msg);                                                   
+                                                                                                   
+    return true;                                                                                   
+}                                                                                                  
+
+
                                                                                                 
 
 
@@ -37,13 +91,43 @@ bool encodeDaySchedule(pb_ostream_t *stream, const pb_field_t *field, void * con
         pb_encode_submessage(stream, TariffDaySchedule_fields, &msg);
     }
 
+    {
+        TariffDaySchedule msg = {.hours=2, .minutes=2, .seconds=3, .tariffId=4};
+
+        pb_encode_tag_for_field(stream, field);
+        pb_encode_submessage(stream, TariffDaySchedule_fields, &msg);
+    }
+
+    {
+        TariffDaySchedule msg = {.hours=3, .minutes=2, .seconds=3, .tariffId=4};
+
+        pb_encode_tag_for_field(stream, field);
+        pb_encode_submessage(stream, TariffDaySchedule_fields, &msg);
+    }
+
     return true;
 }
 
 bool encodeDayProfiles(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
 {
     {
-        DayProfile msg = {.identifier=123};
+        DayProfile msg = {.identifier=1};
+        msg.daySchedule.funcs.encode    = &encodeDaySchedule;
+
+        pb_encode_tag_for_field(stream, field);
+        pb_encode_submessage(stream, DayProfile_fields, &msg);
+    }
+
+    {
+        DayProfile msg = {.identifier=2};
+        msg.daySchedule.funcs.encode    = &encodeDaySchedule;
+
+        pb_encode_tag_for_field(stream, field);
+        pb_encode_submessage(stream, DayProfile_fields, &msg);
+    }
+
+    {
+        DayProfile msg = {.identifier=3};
         msg.daySchedule.funcs.encode    = &encodeDaySchedule;
 
         pb_encode_tag_for_field(stream, field);
@@ -96,24 +180,21 @@ int main(int argc, char **argv)
         if (pb_encode(&stream, MeterConfigMessage_fields, &message))
         {
             //
-            // Decode the message from the buffer.
+            // Show the pb data.
             //
             printf("\n");
             printHexData( buffer, bufferSize );
             printf("\n");
-#if 0
         
-            printf("---decoding---\n");
+            //
+            // Decode the message from the buffer.
+            //
             pb_istream_t stream = pb_istream_from_buffer(buffer, bufferSize);
-            AMessage    message2    = AMessage_init_zero;
-            message2.list.funcs.decode  = &decodeList;
-            message2.sub.funcs.decode  = &decodeSub;
-            pb_decode( &stream, AMessage_fields, &message2 );
+            MeterConfigMessage    decoded    = MeterConfigMessage_init_zero;
+            decoded.rateCalendar.funcs.decode  = &decodeRateCalendar;
+            pb_decode( &stream, MeterConfigMessage_fields, &decoded );
 
-            printf("a=%d\n",message2.a);
-            printf("b=%d\n",message2.b);
-            return 0; /* Success */
-#endif
+            return 0;
         }
         else
         {
