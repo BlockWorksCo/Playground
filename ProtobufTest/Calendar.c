d182163e486dce9ca2b5a31b6c53b6fef60536a8
Steve Tickle <Steve@ZooKoo.com>
2018-07-19 19:18:48 +0100

    .

--- ProtobufTest/Calendar.c
+++ ProtobufTest/Calendar.c
@@ -7,6 +7,7 @@
 #include <pb_encode.h>
 #include <pb_decode.h>
 #include "message.pb.h"
+#include "Calendar.pb.h"
 
 
 
@@ -26,127 +27,81 @@ void printHexData(uint8_t* data, uint32_t numberOfBytes)
 }                                                                                               
                                                                                                 
 
-bool decodeSchedule(pb_istream_t *stream, const pb_field_t *field, void **arg)
-{
-    printf("decodeSchedule\n");
-    
-    Schedule    msg = Schedule_init_zero;
-    pb_decode(stream, Schedule_fields, &msg);
-    
-    printf("\tstart=%d script=%d\n",msg.start,msg.scriptId);
-    return true;
-}
 
-bool decodeSub(pb_istream_t *stream, const pb_field_t *field, void **arg)
-{
-    ASubMessage msg = ASubMessage_init_zero;
-
-    printf("decodeSub\n");
-    msg.schedule.funcs.decode  = &decodeSchedule;
-    msg.schedule.arg  = NULL;
-    pb_decode(stream, ASubMessage_fields, &msg);
-    printf("   one=%d two=%d\n",msg.one,msg.two);
-
-    return true;
-}
-
-bool decodeList(pb_istream_t *stream, const pb_field_t *field, void **arg)
-{
-    uint64_t value  = 0;
-    pb_decode_varint(stream, &value);
-    printf("[%ld]\n", value);
-
-    return true;
-}
-
-
-bool encodeList(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
-{
-    for(uint32_t i=0; i<5; i++)
-    {
-        if (!pb_encode_tag_for_field(stream, field))
-        {
-            return false;
-        }
-        pb_encode_varint(stream, 9+i);
-    }
-    
-    return true;
-}
-
-
-bool encodeSchedules(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
+bool encodeDaySchedule(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
 {
     {
-        Schedule msg = {.start=999,.scriptId=876};
+        TariffDaySchedule msg = {.hours=1, .minutes=2, .seconds=3, .tariffId=4};
+
         pb_encode_tag_for_field(stream, field);
-        pb_encode_submessage(stream, Schedule_fields, &msg);
-    }
-
-    {
-        Schedule msg = {.start=55,.scriptId=66};
-        pb_encode_tag_for_field(stream, field);
-        pb_encode_submessage(stream, Schedule_fields, &msg);
-    }
-
-    {
-        Schedule msg = {.start=77,.scriptId=88};
-        pb_encode_tag_for_field(stream, field);
-        pb_encode_submessage(stream, Schedule_fields, &msg);
+        pb_encode_submessage(stream, TariffDaySchedule_fields, &msg);
     }
 
     return true;
 }
 
-bool encodeSub(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
+bool encodeDayProfiles(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
 {
     {
-        ASubMessage msg = {.one=111,.two=222};
-        pb_encode_tag_for_field(stream, field);
-        pb_encode_submessage(stream, ASubMessage_fields, &msg);
-    }
+        DayProfile msg = {.identifier=123};
+        msg.daySchedule.funcs.encode    = &encodeDaySchedule;
 
-    {
-        ASubMessage msg = {.one=333,.two=444};
-        msg.schedule.funcs.encode   = &encodeSchedules;
-        msg.schedule.arg            = NULL;
         pb_encode_tag_for_field(stream, field);
-        pb_encode_submessage(stream, ASubMessage_fields, &msg);
-    }
-
-    {
-        ASubMessage msg = {.one=555,.two=666};
-        pb_encode_tag_for_field(stream, field);
-        pb_encode_submessage(stream, ASubMessage_fields, &msg);
+        pb_encode_submessage(stream, DayProfile_fields, &msg);
     }
 
     return true;
 }
 
-// 08 FA 01 10 AF 01 18 09 18 0A 18 0B 18 0C 18 0D
+bool encodeRateCalendar(pb_ostream_t *stream, const pb_field_t *field, void * const *arg)
+{
+    {
+        RateCalendar msg = {0};
+        msg.dayProfiles.funcs.encode    = &encodeDayProfiles;
+        msg.dayProfiles.arg             = NULL;
+
+        pb_encode_tag_for_field(stream, field);
+        pb_encode_submessage(stream, RateCalendar_fields, &msg);
+    }
+
+    return true;
+}
+
 int main(int argc, char **argv)
 {
     {
-        AMessage    message = AMessage_init_zero;
+        //
+        // Create a message.
+        //
+        MeterConfigMessage    message = MeterConfigMessage_init_zero;
 
-        message.a   = 0xfa;
-        message.b   = 0xaf;
-        message.has_b    = true;
-        message.list.funcs.encode   = &encodeList;
-        message.sub.funcs.encode     = &encodeSub;
+        message.rateCalendar.funcs.encode   = &encodeRateCalendar;
 
+        //
+        // Get the size of the encoded message.
+        //
         size_t  bufferSize  = 0;
-        pb_get_encoded_size( &bufferSize, AMessage_fields, &message );
+        pb_get_encoded_size( &bufferSize, MeterConfigMessage_fields, &message );
         printf("encoded size = %zu\n", bufferSize);
+
+        //
+        // Allocate a buffer to hold the encoded message.
+        //
         uint8_t*    buffer  = malloc(bufferSize);
         pb_ostream_t stream = pb_ostream_from_buffer(buffer, bufferSize );
 
-        /* Now encode it and check if we succeeded. */
-        if (pb_encode(&stream, AMessage_fields, &message))
+        //
+        // Encode the message into the buffer.
+        //
+        if (pb_encode(&stream, MeterConfigMessage_fields, &message))
         {
+            //
+            // Decode the message from the buffer.
+            //
             printf("\n");
             printHexData( buffer, bufferSize );
             printf("\n");
+#if 0
         
             printf("---decoding---\n");
             pb_istream_t stream = pb_istream_from_buffer(buffer, bufferSize);
@@ -158,6 +113,7 @@ int main(int argc, char **argv)
             printf("a=%d\n",message2.a);
             printf("b=%d\n",message2.b);
             return 0; /* Success */
+#endif
         }
         else
         {
