3e035e90b683fd1648736cde3345c4a75b73d838
Steve Tickle <Steve.Tickle@CyanConnode.com>
2018-12-17 21:38:53 +0000

    .

--- NMS/TestDistributeJob.sh
+++ NMS/TestDistributeJob.sh
@@ -7,7 +7,7 @@ dd if=/dev/urandom of=blob bs=128 count=1
 blob="$(base64 --wrap=0 blob)"
 
 # Form the JSON request using the random BLOB.
-echo \{\"TargetList\":\"asdasd\",\"BLOB\":\"${blob}\"\} > job.tmp
+echo \{\"TargetList\":\[1,2,3,4\],\"BLOB\":\"${blob}\"\} > job.tmp
 
 # Execute the request to distribute the data to the list of Nodes.
 response=$(curl -s --header "Content-Type: application/json" --request POST --data `cat job.tmp` http://localhost:5000/API/DistributionJob)
