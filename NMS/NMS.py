c885c298573990601a4a105f2713767bad987573
Steve Tickle <Steve.Tickle@CyanConnode.com>
2018-12-17 21:52:51 +0000

    .

--- NMS/NMS.py
+++ NMS/NMS.py
@@ -15,6 +15,10 @@ app = Flask(__name__)
 
 
 
+def NodeAddressFromID(id):
+    return '2a03:0500:0001::0200::%s'%(id)
+
+
 @app.route('/API/DistributionJob', methods=['POST','GET'])
 def DistributionJob():
     targetList  = request.json['TargetList']
@@ -22,11 +26,13 @@ def DistributionJob():
     print(targetList)
     print(blob)
 
-    tmp = tempfile.NamedTemporaryFile(delete=False,dir='Jobs')
-    with open(tmp.name, 'w') as f:
-        f.write(str(targetList))
-        f.write(blob)
-    f.close()
+    tmp = tempfile.NamedTemporaryFile(delete=False,dir='Jobs',suffix='.Status')
+    for nodeId in targetList:
+        nodeAddress = NodeAddressFromID( nodeId )
+        with open(tmp.name+'_%s.Request'%(nodeAddress), 'w') as f:
+            f.write(str(targetList))
+            f.write(blob)
+        f.close()
 
     response = jsonify({'JobID':tmp.name})
     response.status_code = 200
