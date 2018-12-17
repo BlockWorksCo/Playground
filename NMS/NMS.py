26d34ad9d2a1913ac5df6d9cec1a657ce31d9193
Steve Tickle <Steve.Tickle@CyanConnode.com>
2018-12-17 20:34:33 +0000

    .

--- NMS/NMS.py
+++ NMS/NMS.py
@@ -1,39 +1,26 @@
 #!/usr/bin/env python3
 
+#
+#
+#
+#
+#
 
 
 from flask import Flask, render_template, send_from_directory, request
-import syslog
 
 
 
 app = Flask(__name__)
 
-@app.route('/stuff/<path:path>')
-def send_js(path):
-    return send_from_directory('stuff', path)
 
 
-@app.route("/Index")
-def Index():
-    return render_template('Index.html')
-
-
-
-@app.route('/Log', methods=['POST'])
+@app.route('/API/DistributionJob', methods=['POST','GET'])
 def Log():
-    print( '%s'%(request.form['message']) )
-    syslog.syslog(syslog.LOG_INFO, request.form['message'])
-
-    return 'Done'
-
-@app.route("/")
-def main():
-    return "Welcome!"
+    return '{Done}'
 
 
 if __name__ == "__main__":
-    syslog.openlog(logoption=syslog.LOG_PID, ident='SUS')
     app.run()
 
 
