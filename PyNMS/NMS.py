e1527cb796bebaa8a8a6ef453fdd7c655d3550cb
Steve Tickle <Steve.Tickle@CyanConnode.com>
2019-04-18 17:21:53 +0100

    .

--- PyNMS/NMS.py
+++ PyNMS/NMS.py
@@ -1,3 +1,11 @@
+"""
+#define TLS_AUTHENTICATION "ECDHE-ECDSA"
+
+#define TLS_ENCRYPTION "AES128-CCM-8"
+
+#define TLS_CIPHER_SUITE TLS_AUTHENTICATION "-" TLS_ENCRYPTION
+
+"""
 
 
 
@@ -10,9 +18,15 @@ from dtls import do_patch
 
 
 do_patch()
-sock = ssl.wrap_socket(socket(AF_INET, SOCK_DGRAM))
-sock.connect(('172.16.32.106', 5684))
-sock.send('Hi there')
+
+# https://docs.python.org/3/library/ssl.html#ssl.SSLContext.set_ciphers
+context = ssl.create_default_context()
+context.set_ciphers('ECDHE-ECDSA-AES128-CCM-8')
+
+sock = socket(AF_INET, SOCK_DGRAM)
+sslsock = ssl.wrap_socket(sock)
+sslsock.connect(('172.16.32.106', 5684))
+sslsock.send('Hi there')
 
 
 
