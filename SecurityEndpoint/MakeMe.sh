a28cb56207c4dd685eab3389d08e4a269ff6b2f0
Steve Tickle <Steve.Tickle@CyanConnode.com>
2019-04-24 16:37:09 +0100

    .

--- SecurityEndpoint/MakeMe.sh
+++ SecurityEndpoint/MakeMe.sh
@@ -2,7 +2,8 @@
 
 
 cd mbedtls-2.16.1
-CFLAGS="-I$PWD/configs -DMBEDTLS_CONFIG_FILE='<config-ccm-ecdhe-tls1_2.h>'" make
+CFLAGS="-I$PWD/configs -DMBEDTLS_CONFIG_FILE='<config-ccm-ecdhe-tls1_2.h>'" make clean all
 cd ..
 
+gcc -o SecurityEndpoint SecurityEndpoint.c  -Imbedtls-2.16.1/include  -Lmbedtls-2.16.1/library -lmbedtls -lmbedcrypto -lmbedx509
 
