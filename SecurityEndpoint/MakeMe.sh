96dfb4fe977c0a663ae761005fe4ac758a899978
Steve Tickle <Steve.Tickle@CyanConnode.com>
2019-04-24 23:03:44 +0100

    .

--- SecurityEndpoint/MakeMe.sh
+++ SecurityEndpoint/MakeMe.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
 
+cd libcoap
+./autogen.sh
+./configure --disable-shared
+make
+cd ..
+
+
 cd mbedtls-2.16.1
 CFLAGS="-I$PWD/configs -DMBEDTLS_CONFIG_FILE='<config-ccm-ecdhe-tls1_2.h>'" make clean all
 cd ..
