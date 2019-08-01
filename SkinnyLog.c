8217c8d5456d2fa4765897d9f1c284e22abb9c24
Steve Tickle <Steve.Tickle@CyanConnode.com>
2019-08-01 23:34:30 +0100

    .

---
+++ SkinnyLog.c
@@ -0,0 +1,38 @@
+
+
+
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <string.h>
+
+
+
+
+
+void lprintf(const char* format, ...)
+{
+    va_list ap;
+
+    va_start(ap, format);
+
+    for(uint32_t i=0; i<strlen(format); i++ ) {
+        if( format[i] == '%' ) {
+            uint32_t    value   = va_arg(ap, uint32_t);
+            printf("value = %08x\n", value);
+        }
+    }
+
+
+    va_end(ap);
+}
+
+
+
+
+int main()
+{
+    lprintf("  [%d]   -%d-   %d",1,2,3);
+}
+
+
