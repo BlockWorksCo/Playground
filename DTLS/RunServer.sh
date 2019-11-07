97150d2dcb00cfe029171eb566aba973f584c1f2
Steve Tickle <Steve.Tickle@CyanConnode.com>
2019-11-07 23:01:47 +0000

    .

--- DTLS/RunServer.sh
+++ DTLS/RunServer.sh
@@ -1,5 +1,5 @@
 
 
-openssl s_server -dtls1_2 -cert cert.pem -key key.pem -accept 4444 -psk abc123 -cipher PSK-AES128-CCM8
+openssl s_server -dtls1_2 -cert cert.pem -key key.pem -accept 4444 -cipher ECDHE-ECDSA-AES128-CCM8
 
 
