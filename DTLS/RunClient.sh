09ad882e7101f7985be734a266e9151086a3175a
Steve Tickle <Steve.Tickle@CyanConnode.com>
2019-11-07 22:57:49 +0000

    .

--- DTLS/RunClient.sh
+++ DTLS/RunClient.sh
@@ -1,7 +1,7 @@
 
 
 
-openssl s_client -dtls1_2 -connect 127.0.0.1:4444 -debug -cert cert.pem -key key.pem
+openssl s_client -dtls1_2 -connect 127.0.0.1:4444 -debug -cert cert.pem -key key.pem -psk abc123 -cipher PSK-AES128-CCM8
 
 
 
