ee39809500da7d25eb8254119bf36d800c03e2ca
Steve Tickle <Steve.Tickle@CyanConnode.com>
2019-12-22 22:59:02 +0000

    .

---
+++ BLE/Applications/TestThree/main.c
@@ -0,0 +1,22 @@
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
+#include "nordic_common.h"
+#include "nrf.h"
+
+
+#define APP_TIMER_PRESCALER             0                                           /**< Value of the RTC1 PRESCALER register. */
+#define APP_TIMER_OP_QUEUE_SIZE         4                                           /**< Size of timer operation queues. */
+
+
+/**@brief Application main function.
+ */
+int main(void)
+{
+    // Enter main loop.
+    for (;;)
+    {
+    }
+}
+
