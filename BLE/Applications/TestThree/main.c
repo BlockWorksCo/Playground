5b43c26f3d957bcc44cced7ace336e50d42e5d0e
Steve Tickle <Steve.Tickle@CyanConnode.com>
2019-12-23 23:28:40 +0000

    .

--- BLE/Applications/TestThree/main.c
+++ BLE/Applications/TestThree/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include "nordic_common.h"
 #include "nrf.h"
+#include "nrf_gpio.h"
 
 
 
@@ -11,9 +12,13 @@
  */
 int main(void)
 {
+    nrf_gpio_cfg_output(1);
+
     // Enter main loop.
     for (;;)
     {
+        nrf_gpio_pin_set(1);
+        nrf_gpio_pin_clear(1);
     }
 }
 
