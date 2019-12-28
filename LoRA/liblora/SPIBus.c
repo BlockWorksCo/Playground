417d9f897941e3ac3907bb65dbdb39367a1795e2
Steve Tickle <Steve.Tickle@CyanConnode.com>
2019-12-28 20:48:23 +0000

    .

--- LoRA/liblora/SPIBus.c
+++ LoRA/liblora/SPIBus.c
@@ -31,6 +31,33 @@ void spiBusInit()
     GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOB, &GPIO_InitStructure);
 
+    /*
+     * configure pins used for SPI1
+     * PA.04 = CS
+     * PA.05 = SCK
+     * PA.06 = MISO (Master In/Slave Out)
+     * PA.07 = MOSI (Master Out/Slave In)
+     */
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA | RCC_APB2Periph_AFIO, ENABLE);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7 | GPIO_Pin_5;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /*
+     * The MISO, MOSI and CLK pins are now connected to their AF
+     * so that the SPI1 can take over control of the pins
+     */
+    GPIO_PinLockConfig(GPIOA, GPIO_Pin_5);
+    GPIO_PinLockConfig(GPIOA, GPIO_Pin_6);
+    GPIO_PinLockConfig(GPIOA, GPIO_Pin_7);
+
     //
     // Init the SPI1 peripheral.
     //
@@ -39,12 +66,11 @@ void spiBusInit()
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_SPI1, ENABLE);
 
     SPI_InitStructure.SPI_Direction = SPI_Direction_2Lines_FullDuplex;
-    SPI_InitStructure.SPI_Direction = SPI_Direction_1Line_Tx;
     SPI_InitStructure.SPI_DataSize = SPI_DataSize_8b;
     SPI_InitStructure.SPI_CPOL = SPI_CPOL_Low;
     SPI_InitStructure.SPI_CPHA = SPI_CPHA_1Edge;
     SPI_InitStructure.SPI_NSS = SPI_NSS_Soft;
-    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_2;
+    SPI_InitStructure.SPI_BaudRatePrescaler = SPI_BaudRatePrescaler_64;
     SPI_InitStructure.SPI_FirstBit = SPI_FirstBit_MSB;
     SPI_InitStructure.SPI_Mode = SPI_Mode_Master;
     SPI_Init(SPI1, &SPI_InitStructure);
