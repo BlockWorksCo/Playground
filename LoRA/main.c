06cd61ec81312dbb7893e70b0cb88f9f964f7e0f
Steve Tickle <Steve.Tickle@CyanConnode.com>
2019-12-31 11:08:50 +0000

    .

--- LoRA/main.c
+++ LoRA/main.c
@@ -44,38 +44,38 @@ void loraTransmitPacket( SPISlaveID id, uint8_t* buf, uint8_t size )
 
 int main(void)
 {
-  GPIO_InitTypeDef  GPIO_InitStructure;
+    GPIO_InitTypeDef  GPIO_InitStructure;
 
-  /* Initialize LED which connected to PC13 */
-  // Enable PORTC Clock
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-  /* Configure the GPIO_LED pin */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOC, &GPIO_InitStructure);
+    /* Initialize LED which connected to PC13 */
+    // Enable PORTC Clock
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
+    /* Configure the GPIO_LED pin */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOC, &GPIO_InitStructure);
 
-  /* Initialize LED which connected to PC13 */
-  // Enable PORTC Clock
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-  /* Configure the GPIO_LED pin */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14|GPIO_Pin_15;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
+    /* Initialize LED which connected to PC13 */
+    // Enable PORTC Clock
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    /* Configure the GPIO_LED pin */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14|GPIO_Pin_15;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
 
-  GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Set C13 to Low level ("0")
-  GPIO_ResetBits(GPIOB, GPIO_Pin_14); // Set C13 to Low level ("0")
-  GPIO_ResetBits(GPIOB, GPIO_Pin_15); // Set C13 to Low level ("0")
+    GPIO_ResetBits(GPIOC, GPIO_Pin_13); // Set C13 to Low level ("0")
+    GPIO_ResetBits(GPIOB, GPIO_Pin_14); // Set C13 to Low level ("0")
+    GPIO_ResetBits(GPIOB, GPIO_Pin_15); // Set C13 to Low level ("0")
 
-  /* Initialize Button input PB10 PB11*/
-  // Enable PORTB Clock
-  RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
-  /* Configure the GPIO_BUTTON pin */
-  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10|GPIO_Pin_11;
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
-  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
-  GPIO_Init(GPIOB, &GPIO_InitStructure);
+    /* Initialize Button input PB10 PB11*/
+    // Enable PORTB Clock
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOB, ENABLE);
+    /* Configure the GPIO_BUTTON pin */
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_10|GPIO_Pin_11;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_2MHz;
+    GPIO_Init(GPIOB, &GPIO_InitStructure);
 
     spiBusInit();
 
@@ -91,20 +91,20 @@ int main(void)
     RFM96_LoRaEntryRx( SlaveA );
     RFM96_LoRaEntryRx( SlaveB );
 
-  while (1) {
+    while (1) {
 
-    	/* Toggle LED which connected to PC13*/
-    	GPIOC->ODR ^= GPIO_Pin_13; // Invert C13
+        /* Toggle LED which connected to PC13*/
+        GPIOC->ODR ^= GPIO_Pin_13; // Invert C13
 
-    	/* delay */
+        /* delay */
         delay_ms(100);
 
-    	/* Toggle LED which connected to PC13*/
-    	GPIOC->ODR ^= GPIO_Pin_13; // Invert C13
+        /* Toggle LED which connected to PC13*/
+        GPIOC->ODR ^= GPIO_Pin_13; // Invert C13
 
         ReadTemperature();
 
-    	/* delay */
+        /* delay */
         delay_ms(100);
 
         //
@@ -164,5 +164,5 @@ int main(void)
                 loraTransmitPacket( SlaveB,  &packet[0], sizeof(packet) );
             }
         }
-  }
+    }
 }
