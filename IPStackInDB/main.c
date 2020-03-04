dc1903ada6919e7a3947ede8704cd6b5e98e2a9f
Steve Tickle <Steve.Tickle@CyanConnode.com>
2020-03-04 05:56:32 +0000

    .

--- IPStackInDB/main.c
+++ IPStackInDB/main.c
@@ -22,6 +22,9 @@
 #include "ipv6.h"
 #include "PersistentQueue.h"
 
+//#include <wolfssl/options.h>
+#include <wolfssl/ssl.h>
+
 /* buffer for reading from tun/tap interface, must be >= 1500 */
 #define BUFSIZE 2000
 #define CLIENT 0
@@ -49,6 +52,15 @@ void processIPv6Packet( uint8_t* data, size_t numberOfBytes )
 }
 
 
+int myCBIORecv(WOLFSSL *ssl, char *buf, int sz, void *ctx)
+{
+}
+
+int myCBIOSend(WOLFSSL *ssl, char *buf, int sz, void *ctx)
+{
+}
+
+
 int main(int argc, char* argv[])
 {
     int option;
@@ -61,6 +73,80 @@ int main(int argc, char* argv[])
     //
     pqInit(0, BUFSIZE, 16, processIPv6Packet);
     pqInit(1, BUFSIZE, 16, transmitIPv6Packet);
+
+    //
+    wolfSSL_Init();
+
+    /* Set ctx to DTLS 1.2 */
+    WOLFSSL_CTX* ctx;
+    if ((ctx = wolfSSL_CTX_new(wolfDTLSv1_2_server_method())) == NULL) {
+        printf("wolfSSL_CTX_new error.\n");
+        return 1;
+    }
+
+    char        caCertLoc[] = "../certs/ca-cert.pem";
+    char        servCertLoc[] = "../certs/server-cert.pem";
+    char        servKeyLoc[] = "../certs/server-key.pem";
+
+    /* Load CA certificates */
+    if (wolfSSL_CTX_load_verify_locations(ctx,caCertLoc,0) !=
+            SSL_SUCCESS) {
+        printf("Error loading %s, please check the file.\n", caCertLoc);
+        return 1;
+    }
+
+    /* Load server certificates */
+    if (wolfSSL_CTX_use_certificate_file(ctx, servCertLoc, SSL_FILETYPE_PEM) != 
+                                                                 SSL_SUCCESS) {
+        printf("Error loading %s, please check the file.\n", servCertLoc);
+        return 1;
+    }
+
+    /* Load server Keys */
+    if (wolfSSL_CTX_use_PrivateKey_file(ctx, servKeyLoc,
+                SSL_FILETYPE_PEM) != SSL_SUCCESS) {
+        printf("Error loading %s, please check the file.\n", servKeyLoc);
+        return 1;
+    }
+
+    /* Create the WOLFSSL Object */
+    WOLFSSL*      ssl = NULL;
+    int         cont = 0;
+    static int cleanup = 0;
+    if ((ssl = wolfSSL_new(ctx)) == NULL) {
+        printf("wolfSSL_new error.\n");
+        cleanup = 1;
+        cont = 1;
+    }
+
+    //
+    wolfSSL_SetIORecv(ctx, myCBIORecv);
+    wolfSSL_SetIOSend(ctx, myCBIOSend);
+
+    wolfSSL_SetIOReadCtx(ssl, NULL);
+    wolfSSL_SetIOWriteCtx(ssl, NULL);
+    wolfSSL_set_using_nonblock(ssl, 1);
+
+
+    // turn on EC support
+    wolfSSL_CTX_UseSupportedCurve(ctx, WOLFSSL_ECC_SECP256R1);
+    // turn on trusted_ca_keys extension support
+    //wolfSSL_CTX_UseTrustedCaKeys(ctx);
+
+    // limit the list of supported suites
+    wolfSSL_CTX_set_cipher_list(ctx, "ECDHE-ECDSA-AES128-CCM-8");
+
+    //
+    wolfSSL_set_verify(ssl, SSL_VERIFY_PEER | SSL_VERIFY_FAIL_IF_NO_PEER_CERT, NULL);
+
+
+    //
+    int res = wolfSSL_negotiate(ssl);
+
+    if( wolfSSL_is_init_finished(ssl) != 0 ) {
+        printf("DTLS session negotiated.\n");
+    }
+
 
     /* initialize tun/tap interface */
     if ( (tun_fd = tun_alloc(if_name, flags | IFF_NO_PI)) < 0 )
