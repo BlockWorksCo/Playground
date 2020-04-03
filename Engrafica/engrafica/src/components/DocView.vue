f8c106a67618b0236d07b24974c9aa94ff14deb4
Steve Tickle <steve.tickle@cyanconnode.com>
2020-04-03 17:03:43 +0100

    .

--- Engrafica/engrafica/src/components/DocView.vue
+++ Engrafica/engrafica/src/components/DocView.vue
@@ -1,6 +1,36 @@
 <template>
   <v-container fluid>
-    <pdf src="https://0.0.0.0:8081/MAX7400-MAX7407.pdf" :page="1">
+
+    <div id="buttons" class="ui grey three item inverted bottom fixed menu transition hidden">
+      <a class="item" @click="page > 1 ? page-- : 1">
+        <i class="left chevron icon"></i>
+        Back
+      </a>
+      <a class="ui active item">
+        {{page}} / {{ numPages ? numPages : '∞' }}
+      </a>
+      <a class="item" @click="page < numPages ? page++ : 1">
+        Forward
+        <i class="right chevron icon"></i>
+      </a>
+    </div>
+
+    <div id="buttons" class="ui grey three item inverted bottom fixed menu transition hidden">
+      <a class="item" @click="scale -= scale > 0.2 ? 0.1 : 0">
+        <i class="left chevron icon" />
+          Zoom -
+      </a>
+      <a class="ui active item">
+        {{ formattedZoom }} %
+      </a>
+      <a class="item" @click="scale += scale < 2 ? 0.1 : 0">
+        Zoom +
+        <i class="right chevron icon" />
+      </a>
+    </div>
+
+    <pdf src="https://0.0.0.0:8081/MAX7400-MAX7407.pdf" v-for="i in numPages" :key="i" :id="i" :page="i"
+      :scale.sync="scale" style="width:100%;margin:20px auto;">
     <template slot="loading">
       loading content here...
     </template>
@@ -10,69 +40,110 @@
 </template>
 
 <script>
+
+ // https://github.com/arkokoley/pdfvuer
  
- import pdf from 'pdfvuer'
+ import pdfvuer from 'pdfvuer'
+ import $ from 'jquery'
 
   export default {
+    //
     name: 'DocView',
 
+    //
     components: {
-		pdf
+		pdf: pdfvuer
 	},
 
+    //
     data: () => ({
-      currentPage: 0,
-      pageCount: 0,
-      ecosystem: [
-        {
-          text: 'vuetify-loader',
-          href: 'https://github.com/vuetifyjs/vuetify-loader',
-        },
-        {
-          text: 'github',
-          href: 'https://github.com/vuetifyjs/vuetify',
-        },
-        {
-          text: 'awesome-vuetify',
-          href: 'https://github.com/vuetifyjs/awesome-vuetify',
-        },
-      ],
-      importantLinks: [
-        {
-          text: 'Documentation',
-          href: 'https://vuetifyjs.com',
-        },
-        {
-          text: 'Chat',
-          href: 'https://community.vuetifyjs.com',
-        },
-        {
-          text: 'Made with Vuetify',
-          href: 'https://madewithvuejs.com/vuetify',
-        },
-        {
-          text: 'Twitter',
-          href: 'https://twitter.com/vuetifyjs',
-        },
-        {
-          text: 'Articles',
-          href: 'https://medium.com/vuetify',
-        },
-      ],
-      whatsNext: [
-        {
-          text: 'Explore components',
-          href: 'https://vuetifyjs.com/components/api-explorer',
-        },
-        {
-          text: 'Select a layout',
-          href: 'https://vuetifyjs.com/layout/pre-defined',
-        },
-        {
-          text: 'Frequently Asked Questions',
-          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-        },
-      ],
+      page: 1,
+      numPages: 3,
+      scale: 'page-width',
+
+
     }),
+
+    //
+  computed: {
+    formattedZoom () {
+        return Number.parseInt(this.scale * 100);
+    },
+  },
+
+    //
+  mounted () {
+    this.getPdf()
+  },
+
+    //
+  watch: {
+    show: function (s) {
+      if(s) {
+        this.getPdf();
+      }
+    },
+
+    //
+    page: function (p) {
+      if( window.pageYOffset <= this.findPos(document.getElementById(p)) || ( document.getElementById(p+1) && window.pageYOffset >= this.findPos(document.getElementById(p+1)) )) {
+        // window.scrollTo(0,this.findPos(document.getElementById(p)));
+        document.getElementById(p).scrollIntoView();
+      }
+    }
+  },
+
+
+    //
+    methods: {
+
+        getPdf () {
+          var self = this;
+          self.pdfdata = pdfvuer.createLoadingTask('./static/relativity.pdf');
+          self.pdfdata.then(pdf => {
+            self.numPages = pdf.numPages;
+            window.onscroll = function() { 
+              changePage() 
+              stickyNav()  
+            }
+
+            // Get the offset position of the navbar
+            var sticky = $('#buttons')[0].offsetTop
+
+            // Add the sticky class to the self.$refs.nav when you reach its scroll position. Remove "sticky" when you leave the scroll position
+            function stickyNav() {
+              if (window.pageYOffset >= sticky) {
+                $('#buttons')[0].classList.remove("hidden")
+              } else {
+                $('#buttons')[0].classList.add("hidden")
+              }
+            }
+
+            function changePage () {
+              var i = 1, count = Number(pdf.numPages);
+              do {
+                if(window.pageYOffset >= self.findPos(document.getElementById(i)) && 
+                    window.pageYOffset <= self.findPos(document.getElementById(i+1))) {
+                  self.page = i
+                }
+                i++
+              } while ( i < count)
+              if (window.pageYOffset >= self.findPos(document.getElementById(i))) {
+                self.page = i
+              }
+            }
+          });
+        },
+
+        //
+        findPos(obj) {
+          return obj.offsetTop;
+        },
+       }
+
+
   }
 </script>
+
+
+
