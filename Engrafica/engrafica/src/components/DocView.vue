fd3e100c76efb09cdf32c62f4fa33500f61a1179
Steve Tickle <Steve.Tickle@CyanConnode.com>
2020-04-05 00:02:20 +0100

    .

--- Engrafica/engrafica/src/components/DocView.vue
+++ Engrafica/engrafica/src/components/DocView.vue
@@ -36,6 +36,45 @@
     </template>
   </pdf>
 
+<v-speed-dial fab top right open-on-hover fixed direction="left" >
+      <template v-slot:activator>
+        <v-btn
+          v-model="fab"
+          color="blue darken-2"
+          dark
+          fab
+        >
+          <v-icon v-if="fab">mdi-close</v-icon>
+          <v-icon v-else>mdi-account-circle</v-icon>
+        </v-btn>
+      </template>
+      <v-btn
+        fab
+        dark
+        small
+        color="green"
+      >
+        <v-icon>mdi-pencil</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="indigo"
+      >
+        <v-icon>mdi-plus</v-icon>
+      </v-btn>
+      <v-btn
+        fab
+        dark
+        small
+        color="red"
+      >
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
+    </v-speed-dial>
+
+
   </v-container>
 </template>
 
@@ -62,13 +101,36 @@
       numPages: 0,
       scale: 'page-width',
 
+      direction: 'top',
+      fab: false,
+      fling: false,
+      hover: false,
+      tabs: null,
+      top: false,
+      right: true,
+      bottom: true,
+      left: false,
+      transition: 'slide-y-reverse-transition',
     }),
 
     //
     computed: {
+
+      //
       formattedZoom () {
         return Number.parseInt(this.scale * 100);
       },
+
+      //
+      activeFab () {
+        switch (this.tabs) {
+          case 'one': return { class: 'purple', icon: 'account_circle' }
+          case 'two': return { class: 'red', icon: 'edit' }
+          case 'three': return { class: 'green', icon: 'keyboard_arrow_up' }
+          default: return {}
+        }
+      },
+
     },
 
     //
